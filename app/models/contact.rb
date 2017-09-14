2fc550de369aaf646aeddde84dd4affbdb8b9525
william <william.mccumstie@outlook.com>
2017-09-14 15:48:28 +0100

    Add basic validation to contacts
    Basic validation has been added to contacts to check all the fields
    have been supplied. The email is also validated with a email validation
    gem

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,6 @@
 class Contact < ApplicationRecord
+  validates :name, presence: true
+  validates :email, email_format: { message: 'is not looking good' }
+  validates :username, presence: true
+  validates :password, presence: true, length: { minimum: 5 }
 end
