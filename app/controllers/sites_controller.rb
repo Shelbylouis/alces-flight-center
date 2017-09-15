3c255436509cd351c199de5ac436a9fdf26c8ee9
william <william.mccumstie@outlook.com>
2017-09-15 13:30:33 +0100

    Create the show/new Site pages

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -3,11 +3,26 @@ class SitesController < ApplicationController
     @sites = Site.all
   end
 
+  def show
+    @site = Site.find(params[:id])
+  end
+
   def new
     @site = Site.new
   end
 
+  def create
+    @site = Site.new(site_params)
+    if @site.save
+      redirect_to @site
+    else
+      render 'new'
+    end
+  end
+
   private
 
-  
+  def site_params
+    params.require(:site).permit(:name, :description)
+  end
 end
