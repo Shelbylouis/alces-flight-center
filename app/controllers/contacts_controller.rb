eefab1c93f0318418a30ce631cb661d963af171f
william <william.mccumstie@outlook.com>
2017-09-15 18:21:19 +0100

    Fix broken contact links and delete
    A whole bunch of the contact paths have been broken because of the
    API change due to Site. This commit fixes this and allows contacts
    to be edited and deleted

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,51 +1,59 @@
 class ContactsController < ApplicationController
   def index
-    @site = Site.find(params[:site_id])
+    @site = find_site
     @contacts = @site.contacts.all
   end
 
   def show
+    @site = find_site
     @contact = Contact.find(params[:id])
   end
 
   def new
-    @site = Site.find(params[:site_id])
+    @site = find_site
     @contact = @site.contacts.build
   end
 
   def edit
+    @site = find_site
     @contact = Contact.find(params[:id])
   end
 
   def create
-    @site = Site.find(params[:site_id])
+    @site = find_site
     @contact = @site.contacts.new(contact_params)
     if @contact.save
-      redirect_to site_contact(@site, @contact)
+      redirect_to [@site, @contact]
     else
       render 'new'
     end
   end
 
   def update
+    @site = find_site
     @contact = Contact.find(params[:id])
     if @contact.update(contact_params)
-      redirect_to @contact
+      redirect_to [@site, @contact]
     else
       render 'edit'
     end
   end
 
   def destroy
+    @site = find_site
     @contact = Contact.find(params[:id])
     @contact.destroy
    
-    redirect_to contacts_path
+    redirect_to @site
   end
 
   private
 
   def contact_params
     params.require(:contact).permit(:name, :email, :username, :password)
+  end
+
+  def find_site
+    Site.find(params[:site_id])
   end
 end
