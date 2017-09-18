0a39bd63ef5fafc611634bd956bf994879fe9b13
william <william.mccumstie@outlook.com>
2017-09-18 14:28:11 +0100

    Allow a Cluster to be added to a site

--- app/controllers/clusters_controller.rb
+++ app/controllers/clusters_controller.rb
@@ -1,13 +1,36 @@
 class ClustersController < ApplicationController
-  
+  def index
+    @site = find_site
+    @clusters = @site.clusters.all
+  end
+
+  def show
+    @site = find_site
+    @cluster = Cluster.find(params[:id])
+  end
+
   def new
     @site = find_site
     @cluster = @site.clusters.build
+  end
+
+  def create
+    @site = find_site
+    @cluster = @site.clusters.new(cluster_params)
+    if @cluster.save
+      redirect_to [@site, @cluster]
+    else
+      render 'new'
+    end
   end
 
   private
 
   def find_site
     Site.find(params[:site_id])
+  end
+
+  def cluster_params
+    params.require(:cluster).permit(:name, :description, :support_type)
   end
 end
