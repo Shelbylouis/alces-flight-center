a3de94226c4b91203146f2aade8a82b871759042
william <william.mccumstie@outlook.com>
2017-09-18 17:17:26 +0100

    Create a new commponent and show it in cluster page

--- app/controllers/components_controller.rb
+++ app/controllers/components_controller.rb
@@ -1,14 +1,33 @@
 class ComponentsController < ApplicationController
 
+  def show
+    @component = Component.find(params[:id])
+    @cluster = find_cluster(@component)
+  end
+
   def new
     @cluster = find_cluster
     @component = @cluster.components.build
   end
 
-  private
-
-  def find_cluster
-    Cluster.find(params[:cluster_id])
+  def create
+    @cluster = find_cluster
+    @component = @cluster.components.new(component_params)
+    if @component.save
+      redirect_to [@cluster, @component]
+    else
+      render 'new'
+    end
   end
 
+  private
+
+  def find_cluster(component = nil)
+    cluster_id = (component ? component.cluster.id : params[:cluster_id])
+    Cluster.find(cluster_id)
+  end
+
+  def component_params
+    params.require(:component).permit(:name, :description, :component_type)
+  end
 end
