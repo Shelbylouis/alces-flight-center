6f281030661575eb7c59640359442a1fdf951187
Bob Whitelock <bob.whitelock1@gmail.com>
2017-09-26 14:58:45 +0100

    Add distinct landing pages when signed in/out

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  root 'home#show'
 
-  resources :cases, except: [:destroy]
+  constraints Clearance::Constraints::SignedIn.new do
+    root 'home#show'
+    resources :cases, except: [:destroy]
+  end
+
+  constraints Clearance::Constraints::SignedOut.new do
+    root 'home#signed_out'
+  end
 end
