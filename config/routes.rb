bd84ef3c39501f10ce0770bac60a847b5f9d9f14
Bob Whitelock <bob.whitelock1@gmail.com>
2017-09-27 17:00:28 +0100

    Display sign out button in admin dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
+  # To display a working link to sign users out of the admin dashboard,
+  # rails-admin expects a `logout_path` route helper to exist which will sign
+  # them out; this declaration defines this.
+  delete :logout, to: 'clearance/sessions#destroy'
+
   constraints Clearance::Constraints::SignedIn.new do
     root 'cases#index'
     resources :cases, except: [:destroy]
