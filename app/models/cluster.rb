43fbef9fd274152c8044cb53d50c47ddf4a3e819
Bob Whitelock <bob.whitelock1@gmail.com>
2017-09-27 18:57:38 +0100

    Have only valid support types shown and choosable
    Previously anything could be entered for support type in the admin
    interface and you would only find out that an input was invalid on
    submit; now the valid support types are displayed and one of these
    should be selected.

--- app/models/cluster.rb
+++ app/models/cluster.rb
@@ -1,10 +1,17 @@
 class Cluster < ApplicationRecord
   SUPPORT_TYPES = ['managed', 'advice'].freeze
+
   belongs_to :site
   has_many :components, dependent: :destroy
   has_many :cases
 
   validates_associated :site
   validates :name, presence: true
-  validates :support_type, inclusion: { in: SUPPORT_TYPES }
+  validates :support_type, inclusion: { in: SUPPORT_TYPES }, presence: true
+
+  # Automatically picked up by rails_admin so only these options displayed when
+  # selecting support type.
+  def support_type_enum
+    SUPPORT_TYPES
+  end
 end
