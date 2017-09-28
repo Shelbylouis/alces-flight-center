3558a544d82f8275556d641f6a77ab9c24e0d515
william <william.mccumstie@outlook.com>
2017-09-28 17:33:01 +0100

    Create utility for striping the id from a string
    The class strings contain the id at the end. This provides the ability
    to strip the id by looking for the `-id-` part of the string.

--- app/assets/javascripts/new_case_event_handler.js
+++ app/assets/javascripts/new_case_event_handler.js
@@ -1,16 +1,26 @@
 // Filters the components options by case category
 $(function() {
+  function strip_id_from_class(obj) {
+    class_string = obj.className
+    if(!class_string) { return null; } // Handles missing class name
+    tag_without_id = class_string.match(/.*-id-/)[0]
+    return parseInt(class_string.replace(tag_without_id, ''))
+  }
+
   function new_case_update_component_field() {
-    var new_case_category = $('#CaseForm_case_case_category_id').val()
     options = $('#CaseForm_case_component_id').children()
+    categories = $('#CaseForm_case_case_category_id')
+    selected_category_index = parseInt(categories.val()) -1
+    selected_category = categories.children()[selected_category_index]
 
     // Unhides all the options fields
     options.each(function(_index, opt) { $(opt).show() })
 
     // Hide options that do not match the new case category
     options.filter(function(_index, opt) {
-      option_case_category_id = opt.className.replace('data-component-type-id-', '')
-      return new_case_category != option_case_category_id
+      selected_category_id = strip_id_from_class(selected_category)
+      console.log(selected_category_id)
+      return selected_category_id != strip_id_from_class(opt)
     }).each(function(_index, opt){ $(opt).hide() })
   }
 
