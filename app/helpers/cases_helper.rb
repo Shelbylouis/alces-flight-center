e20cbee891f49cbc392423de868afe998c5ad7f5
william <william.mccumstie@outlook.com>
2017-09-28 18:37:43 +0100

    Abstract the creation of options with data attribute
    The code could be abstracted so the options_for_select is called by
    the builds method

--- app/helpers/cases_helper.rb
+++ app/helpers/cases_helper.rb
@@ -1,30 +1,29 @@
 module CasesHelper
   def options_for_site_components
-    arr = build_options_array(@site_components) do |site_component|
+    build_options_with_data_attributes(@site_components) do |site_component|
       { "data-component-type-id": site_component.component_type.id }
     end
-    options_for_select(arr)
   end
 
   def options_for_case_categories
-    arr = build_options_array(@case_categories) do |case_category|
+    build_options_with_data_attributes(@case_categories) do |case_category|
       component_id = case_category.component_type&.id
       component_id ? { 'data-component-type-id': component_id } : {}
     end
-    options_for_select(arr)
   end
 
   private
 
   ##
   # The hash returned from the block becomes the html attributes hash
-  def build_options_array(object_array)
-    object_array.map do |obj|
+  def build_options_with_data_attributes(object_array)
+    arr = object_array.map do |obj|
       [
         obj.name,
         obj.id,
         yield(obj)
       ]
     end
+    options_for_select(arr)
   end
 end
