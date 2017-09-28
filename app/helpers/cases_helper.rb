c6a773eb563c0738157cfe33cb7b0e4e5b6348eb
william <william.mccumstie@outlook.com>
2017-09-28 12:28:28 +0100

    Store ComponentType#id with component form input
    Which components to show in form are going to be filtered on the
    component type. As this occurs client side, the component-type-id is
    required to be stored.
    
    This is done by including it as a class.

--- app/helpers/cases_helper.rb
+++ app/helpers/cases_helper.rb
@@ -1,2 +1,17 @@
 module CasesHelper
+  def options_for_site_components
+    options_for_select(site_components_array)
+  end
+
+  private
+
+  def site_components_array
+    @site_components.map do |site_component|
+      [
+        site_component.name,
+        site_component.id,
+        { class: "data-component-type-id-#{site_component.component_type.id}" }
+      ]
+    end
+  end
 end
