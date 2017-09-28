0f6eeb01dc16eb7e86076c7ffaaee0c1c62007dd
william <william.mccumstie@outlook.com>
2017-09-28 15:01:09 +0100

    Load CaseCategory#id as html class into option tag
    The case-category-id has been included into the options tag as a class
    This means the components can now be filtered against the selected
    case category.
    
    Duplicate code will be fixed in following commit.

--- app/helpers/cases_helper.rb
+++ app/helpers/cases_helper.rb
@@ -3,6 +3,13 @@ module CasesHelper
     options_for_select(site_components_array)
   end
 
+  def options_for_case_categories
+    arr = build_options_array(@case_categories) do |case_category|
+      { class: "data-case-category-id-#{case_category.id}" }
+    end
+    options_for_select(arr)
+  end
+
   private
 
   def site_components_array
@@ -11,6 +18,18 @@ module CasesHelper
         site_component.name,
         site_component.id,
         { class: "data-component-type-id-#{site_component.component_type.id}" }
+      ]
+    end
+  end
+
+  ##
+  # The hash returned from the block becomes the html attributes hash
+  def build_options_array(object_array)
+    object_array.map do |obj|
+      [
+        obj.name,
+        obj.id,
+        yield(obj)
       ]
     end
   end
