662565563120bcde48e0111fb45384ab868ea987
william <william.mccumstie@outlook.com>
2017-09-28 19:20:13 +0100

    Filter component options on the cluster
    The components that can be selected are filtered based on the cluster
    id. Currently this will always remove the blank component which will
    need to be added again.

--- app/assets/javascripts/new_case_event_handler.js
+++ app/assets/javascripts/new_case_event_handler.js
@@ -6,30 +6,52 @@ $(function() {
   }
 
   function options() { return $('#CaseForm_case_component_id').children() }
-  function categories() { return $('#CaseForm_case_case_category_id') }
+
+  function selected(object) {
+    index = parseInt(object.val()) - 1
+    return object.children()[index]
+  }
+
   function selected_category() {
-    index = parseInt(categories().val()) -1
-    return categories().children()[index]
+    return selected($('#CaseForm_case_case_category_id'))
+  }
+
+  function selected_cluster() {
+    return selected($('#CaseForm_case_cluster_id'))
   }
 
   function show_all_components() {
     options().each(function(_index, opt) { $(opt).show() })
   }
 
-  function filter_components_on_selected_case_category() {
+  function filter_on_miss_match(collection, expected_id, callback) {
+    collection.filter(function(_index, item) {
+      return expected_id != callback(item)
+    }).each(function(_index, item) { $(item).hide() })
+  }
+
+  function hide_components_on_selected_case_category() {
     selected_component_type_id = data_attr_id(selected_category(), 'component-type')
 
     // Hides components that do not match the select component type
     if(selected_component_type_id) {
-      options().filter(function(_index, opt) {
-        return selected_component_type_id != data_attr_id(opt, 'component-type')
-      }).each(function(_index, opt){ $(opt).hide() })
+      filter_on_miss_match(options(), selected_component_type_id, function(component) {
+        return data_attr_id(component, 'component-type')
+      })
     }
+  }
+
+  function hide_components_on_selected_cluster() {
+    selected_cluster_id = data_attr_id(selected_cluster(), 'cluster')
+    filter_on_miss_match(options(), selected_cluster_id, function(component) {
+      return data_attr_id(component, 'cluster')
+    })
   }
 
   function event_handler() {
     show_all_components()
-    filter_components_on_selected_case_category()
+    hide_components_on_selected_case_category()
+    hide_components_on_selected_cluster()
   }
 
   // Applies the event handler and runs it on load
