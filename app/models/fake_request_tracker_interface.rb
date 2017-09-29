8f914a4e83b54368f44d7516852e53118845dfc2
Bob Whitelock <bob.whitelock1@gmail.com>
2017-09-29 14:30:34 +0100

    Have Fake rt interface produce incrementing ids
    This means we can use multiple ticket ids produced by the fake rt
    interface in development, without the Rails and DB validations that
    Cases have unique `rt_ticket_id` fields failing.

--- app/models/fake_request_tracker_interface.rb
+++ app/models/fake_request_tracker_interface.rb
@@ -3,6 +3,7 @@
 # requests to the real rt instance; for use in development.
 class FakeRequestTrackerInterface
   def create_ticket(requestor_email:, subject:, text:)
-    RequestTrackerInterface::Ticket.new(10001)
+    current_max_rt_ticket_id = Case.maximum(:rt_ticket_id) || 10000
+    RequestTrackerInterface::Ticket.new(current_max_rt_ticket_id + 1)
   end
 end
--- spec/models/request_tracker_interface_spec.rb
+++ spec/models/request_tracker_interface_spec.rb
@@ -3,19 +3,23 @@ require 'rails_helper'
 RSpec.shared_examples 'Request Tracker interface' do
   subject { described_class.new }
 
-  describe 'create_ticket' do
-    it 'creates a ticket and returns object with id' do
-      VCR.use_cassette('rt_create_ticket', re_record_interval: 7.days) do
-        ticket = subject.create_ticket(
-          requestor_email: 'test@example.com',
-          subject: 'Supportware test ticket - please delete',
-          text: <<-EOF.strip_heredoc
+  let :new_ticket_params do
+    {
+      requestor_email: 'test@example.com',
+      subject: 'Supportware test ticket - please delete',
+      text: <<-EOF.strip_heredoc
             Testing
             multiline
             text
             works
-          EOF
-        )
+      EOF
+    }
+  end
+
+  describe 'create_ticket' do
+    it 'creates a ticket and returns object with id' do
+      VCR.use_cassette('rt_create_ticket', re_record_interval: 7.days) do
+        ticket = subject.create_ticket(new_ticket_params)
 
         # All tickets now have IDs greater than this.
         expect(ticket.id).to be > 10000
@@ -29,5 +33,16 @@ RSpec.describe RequestTrackerInterface do
 end
 
 RSpec.describe FakeRequestTrackerInterface do
-  it_behaves_like 'Request Tracker interface'
+  include_context 'Request Tracker interface'
+
+  describe 'create_ticket' do
+    it 'produces tickets with incrementing IDs' do
+      # Mock that we have a Case with the current maximum rt ticket id.
+      max_rt_ticket_id = 10001
+      allow(Case).to receive(:maximum).and_return(max_rt_ticket_id)
+
+      new_rt_ticket_id = subject.create_ticket(new_ticket_params).id
+      expect(new_rt_ticket_id).to eq(max_rt_ticket_id + 1)
+    end
+  end
 end
