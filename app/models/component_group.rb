81a7fffadd28ba4951d31179f4e75fb77fd0a699
Bob Whitelock <bob.whitelock1@gmail.com>
2017-10-03 17:12:04 +0100

    Support generating many components at once for group
    By setting the `genders_host_range` on a `ComponentGroup` and then
    saving it, many components can be created for a group simultaneously.
    This is done by expanding the host range using `nodeattr`, and then
    creating associated `Component`s for any expanded names which don't yet
    have a `Component` in the group. This works both from the admin
    dashboard and when interacting with a `ComponentGroup` object directly.

--- app/models/component_group.rb
+++ app/models/component_group.rb
@@ -4,4 +4,36 @@ class ComponentGroup < ApplicationRecord
   has_many :components
 
   validates :name, presence: true
+
+  attr_accessor :genders_host_range
+
+  after_save :create_needed_components_for_host_range
+
+  def component_names
+    components.map(&:name)
+  end
+
+  private
+
+  def create_needed_components_for_host_range
+    new_node_names = expanded_genders_host_range - component_names
+
+    new_node_names.map do |name|
+      Component.create!(name: name, component_group: self)
+    end
+  end
+
+  def expanded_genders_host_range
+    # Create a temporary genders file with a single line using the
+    # `genders_host_range` and placeholder attribute, and use `nodeattr` to
+    # expand a list of node names for this attribute.
+    genders_attr = 'group'
+    genders_line = [genders_host_range, genders_attr].join(' ')
+    Tempfile.open do |file|
+      file.write(genders_line)
+      file.flush
+      nodeattr_output = `nodeattr -f #{file.path} -c #{genders_attr}`
+      nodeattr_output.strip.split(',')
+    end
+  end
 end
