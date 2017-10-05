79409c6084d7faf0627dbc303148d13ec6bb3802
Bob Whitelock <bob.whitelock1@gmail.com>
2017-10-05 13:00:30 +0100

    Only initialize case form when element exists
    Previously we would attempt to initialize the case form regardless of
    whether the targeted element existed (e.g. when not on the new case
    page), causing a JavaScript error.

--- app/javascript/packs/new_case_form.js
+++ app/javascript/packs/new_case_form.js
@@ -3,17 +3,18 @@ import Elm from './Main'
 
 const initializeFormApp = () => {
   const target = document.getElementById('new-case-form')
+  if (target) {
+    const loadAttributeJson =
+      attribute => JSON.parse(target.getAttribute(attribute))
 
-  const loadAttributeJson =
-    attribute => JSON.parse(target.getAttribute(attribute))
+    const flags = {
+      clusters: loadAttributeJson('data-clusters'),
+      caseCategories: loadAttributeJson('data-case-categories'),
+      components: loadAttributeJson('data-components'),
+    }
 
-  const flags = {
-    clusters: loadAttributeJson('data-clusters'),
-    caseCategories: loadAttributeJson('data-case-categories'),
-    components: loadAttributeJson('data-components'),
+    Elm.Main.embed(target, flags)
   }
-
-  Elm.Main.embed(target, flags)
 }
 
 document.addEventListener('turbolinks:load', initializeFormApp)
