02e9fd63f71add5d402092af4626371600791968
Bob Whitelock <bob.whitelock1@gmail.com>
2017-10-05 18:52:26 +0100

    Prevent case form being re-initialized

--- app/javascript/packs/new_case_form.js
+++ app/javascript/packs/new_case_form.js
@@ -3,7 +3,12 @@ import Elm from './Main'
 
 const initializeFormApp = () => {
   const target = document.getElementById('new-case-form')
-  if (target) {
+
+  // Only initialize the form app if the target exists (we're on a page
+  // requiring the form) and has no children (indicates app has already been
+  // initialized, e.g. this occurs due to Turbolinks if navigate away and then
+  // back to page using browser back/forward buttons).
+  if (target && !target.hasChildNodes()) {
     const loadAttributeJson =
       attribute => JSON.parse(target.getAttribute(attribute))
 
