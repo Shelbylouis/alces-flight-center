90ea6e31d329040fe11010ea31e43d012559faf2
Bob Whitelock <bob.whitelock1@gmail.com>
2017-10-13 11:35:09 +0100

    Extract Validator class from AssetRecordField

--- app/models/asset_record_field.rb
+++ app/models/asset_record_field.rb
@@ -8,48 +8,10 @@ class AssetRecordField < ApplicationRecord
   # Field value can be an empty string, but not null.
   validates :value, exclusion: { in: [nil] }
 
-  validate :associated_with_component_xor_group
-  validate :definition_associated_with_component_type
-  validate :field_settable_at_level
-  validate :field_not_already_set
+  validates_with Validator
 
   def name
     definition.field_name
-  end
-
-  private
-
-  def associated_with_component_xor_group
-    if !asset
-      errors.add(
-        :base,
-        'must be associated with either component or component group'
-      )
-    elsif component && component_group
-      errors.add(
-        :base,
-        'can only be associated with either component or component group, but not both'
-      )
-    end
-  end
-
-  def definition_associated_with_component_type
-    return unless asset
-
-    if !available_field_definitions.include?(definition)
-      errors.add(
-        :asset_record_field_definition,
-        'is not a field definition associated with component type'
-      )
-    end
-  end
-
-  def available_field_definitions
-    component_type.asset_record_field_definitions
-  end
-
-  def component_type
-    asset&.component_type
   end
 
   # An AssetRecordField should be associated with precisely one Component or
@@ -58,27 +20,7 @@ class AssetRecordField < ApplicationRecord
     component || component_group
   end
 
-  def field_settable_at_level
-    # It is not possible to define an asset record field associated with a
-    # ComponentGroup if the field definition states it is not settable at a
-    # group-level.
-    if component_group && !definition.settable_for_group?
-      errors.add(
-        :asset_record_field_definition,
-        'this field is only settable at the component-level'
-      )
-    end
-  end
-
-  def field_not_already_set
-    return unless asset
-
-    asset_field_definitions = asset.asset_record_fields.map(&:definition)
-    if asset_field_definitions.include?(definition)
-      errors.add(
-        :base,
-        "a field for this definition already exists for this #{asset.readable_model_name}, you should edit the existing field"
-      )
-    end
+  def component_type
+    asset&.component_type
   end
 end
---
+++ app/models/asset_record_field/validator.rb
@@ -0,0 +1,67 @@
+
+class AssetRecordField::Validator < ActiveModel::Validator
+  attr_reader :record
+
+  def validate(record)
+    @record = record
+    validate_associated_with_component_xor_group
+    validate_definition_associated_with_component_type
+    validate_field_settable_at_level
+    validate_field_not_already_set
+  end
+
+  private
+
+  def validate_associated_with_component_xor_group
+    if !record.asset
+      record.errors.add(
+        :base,
+        'must be associated with either component or component group'
+      )
+    elsif record.component && record.component_group
+      record.errors.add(
+        :base,
+        'can only be associated with either component or component group, but not both'
+      )
+    end
+  end
+
+  def validate_definition_associated_with_component_type
+    return unless record.asset
+
+    if !available_field_definitions.include?(record.definition)
+      record.errors.add(
+        :asset_record_field_definition,
+        'is not a field definition associated with component type'
+      )
+    end
+  end
+
+  def available_field_definitions
+    record.component_type.asset_record_field_definitions
+  end
+
+  def validate_field_settable_at_level
+    # It is not possible to define an asset record field associated with a
+    # ComponentGroup if the field definition states it is not settable at a
+    # group-level.
+    if record.component_group && !record.definition.settable_for_group?
+      record.errors.add(
+        :asset_record_field_definition,
+        'this field is only settable at the component-level'
+      )
+    end
+  end
+
+  def validate_field_not_already_set
+    return unless record.asset
+
+    asset_field_definitions = record.asset.asset_record_fields.map(&:definition)
+    if asset_field_definitions.include?(record.definition)
+      record.errors.add(
+        :base,
+        "a field for this definition already exists for this #{record.asset.readable_model_name}, you should edit the existing field"
+      )
+    end
+  end
+end
