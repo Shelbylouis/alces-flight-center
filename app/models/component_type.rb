6462022a5cdbcb1de1a9eb33db7c889de0a4973d
Bob Whitelock <bob.whitelock1@gmail.com>
2017-10-16 12:08:47 +0100

    Remove obsolete has_many relationship
    A `ComponentType` no longer `has_many` `CaseCategory`s; in particular
    this was causing RailsAdmin to error when viewing `ComponentType`s as it
    was expecting such a relationship to exist.

--- app/models/component_type.rb
+++ app/models/component_type.rb
@@ -1,6 +1,5 @@
 class ComponentType < ApplicationRecord
   has_many :component_groups
-  has_many :case_categories
   has_and_belongs_to_many :asset_record_field_definitions
 
   validates :name, presence: true
