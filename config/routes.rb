96a0d73b5d8c71660d7d7190a9f385e6b034370a
Bob Whitelock <bob.whitelock1@gmail.com>
2017-10-17 10:12:45 +0100

    Copy in Clearance routes to adapt
    Clearance recommends explicitly setting its routes
    (https://github.com/thoughtbot/clearance/#routes), and we want to
    disable/tweak these in any case.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,19 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
+  resources :passwords, controller: "clearance/passwords", only: [:create, :new]
+  resource :session, controller: "clearance/sessions", only: [:create]
+
+  resources :users, controller: "clearance/users", only: [:create] do
+    resource :password,
+      controller: "clearance/passwords",
+      only: [:create, :edit, :update]
+  end
+
+  get "/sign_in" => "clearance/sessions#new", as: "sign_in"
+  delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
+  get "/sign_up" => "clearance/users#new", as: "sign_up"
+
   # To display a working link to sign users out of the admin dashboard,
   # rails-admin expects a `logout_path` route helper to exist which will sign
   # them out; this declaration defines this.
