e0179de536d113de18c7c66bbfa4c858c9dc1f3a
Bob Whitelock <bob.whitelock1@gmail.com>
2017-10-17 10:38:56 +0100

    Adapt Clearance routes
    - Delete routes we aren't using and don't want to exist.
    
    - Move routes to only be available when logged in/out/in as admin as
    appropriate, apart from password reset routes as user may get linked
    here and want to reset their password even if they're already signed in
    (for some reason).
    
    - Change routes more suitably for app, so sign-in is root path when
    signed out and do not get redirected to `/session` path on failed
    sign-in, and password reset is done at `reset-password` route.

--- config/routes.rb
+++ config/routes.rb
@@ -2,30 +2,26 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
-  resources :passwords, controller: "clearance/passwords", only: [:create, :new]
-  resource :session, controller: "clearance/sessions", only: [:create]
-
-  resources :users, controller: "clearance/users", only: [:create] do
+  get '/reset-password' => 'clearance/passwords#new', as: 'passwords'
+  post '/reset-password' => 'clearance/passwords#create'
+  resources :users, controller: 'clearance/users' do
     resource :password,
-      controller: "clearance/passwords",
-      only: [:create, :edit, :update]
+      controller: 'clearance/passwords',
+      only: [:edit, :update]
   end
-
-  get "/sign_in" => "clearance/sessions#new", as: "sign_in"
-  delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
-  get "/sign_up" => "clearance/users#new", as: "sign_up"
-
-  # To display a working link to sign users out of the admin dashboard,
-  # rails-admin expects a `logout_path` route helper to exist which will sign
-  # them out; this declaration defines this.
-  delete :logout, to: 'clearance/sessions#destroy'
 
   constraints Clearance::Constraints::SignedIn.new { |user| user.admin? } do
     root 'rails_admin/main#dashboard'
+
+    # To display a working link to sign users out of the admin dashboard,
+    # rails-admin expects a `logout_path` route helper to exist which will sign
+    # them out; this declaration defines this.
+    delete :logout, to: 'clearance/sessions#destroy'
   end
 
   constraints Clearance::Constraints::SignedIn.new do
     root 'home#index'
+    delete '/sign_out' => 'clearance/sessions#destroy', as: 'sign_out'
 
     resources :cases, only: [:new, :index, :create] do
       member do
@@ -38,6 +34,7 @@ Rails.application.routes.draw do
   end
 
   constraints Clearance::Constraints::SignedOut.new do
-    root 'clearance/sessions#new'
+    root 'clearance/sessions#new', as: 'sign_in'
+    post '/' => 'clearance/sessions#create', as: 'session'
   end
 end
