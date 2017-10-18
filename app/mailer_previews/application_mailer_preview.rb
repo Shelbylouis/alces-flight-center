7edaf4050c935e65e17190f5f4eaf1529d8ed608
Bob Whitelock <bob.whitelock1@gmail.com>
2017-10-18 13:19:39 +0100

    Extract superclass to use for mailer previews

---
+++ app/mailer_previews/application_mailer_preview.rb
@@ -0,0 +1,19 @@
+
+class ApplicationMailerPreview
+  private
+
+  def user
+    # `@user_id` comes from `user_id` URL parameter, if given.
+    @user_id ? User.find(@user_id) : mock_user
+  end
+
+  def mock_user
+    FactoryGirl.build(
+      :user,
+      id: 9001,
+      name: 'Some User',
+      email: 'some.user@example.com',
+      confirmation_token: '123456'
+    )
+  end
+end
--- app/mailer_previews/clearance_mailer_preview.rb
+++ app/mailer_previews/clearance_mailer_preview.rb
@@ -1,23 +1,6 @@
 
-class ClearanceMailerPreview
+class ClearanceMailerPreview < ApplicationMailerPreview
   def change_password
     ClearanceMailer.change_password user
-  end
-
-  private
-
-  def user
-    # `@user_id` comes from `user_id` URL parameter, if given.
-    @user_id ? User.find(@user_id) : mock_user
-  end
-
-  def mock_user
-    FactoryGirl.build(
-      :user,
-      id: 9001,
-      name: 'Some User',
-      email: 'some.user@example.com',
-      confirmation_token: '123456'
-    )
   end
 end
