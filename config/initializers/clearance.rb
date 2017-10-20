b81ddc41264c853873db216d768db84408e13ad4
Bob Whitelock <bob.whitelock1@gmail.com>
2017-10-20 12:06:18 +0100

    Update mailer URL/email sender for deployment

--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -1,6 +1,6 @@
 Clearance.configure do |config|
   config.routes = false
-  config.mailer_sender = "reply@example.com" # XXX change
+  config.mailer_sender = 'support@alces-flight.com'
   config.rotate_csrf_on_sign_in = true
   config.allow_sign_up = false
 end
