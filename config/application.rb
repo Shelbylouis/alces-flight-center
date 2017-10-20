6ea13e287d195369af87005fa310cea2065063e3
Bob Whitelock <bob.whitelock1@gmail.com>
2017-10-20 13:31:50 +0100

    Move lib files into app so can find in production
    Apparently Rails 5 makes it so files in `lib` can be auto-loaded in
    development but then won't be in production, causing things to blow up
    at runtime (see
    https://stackoverflow.com/questions/38198668/rails-5-load-lib-files-in-production).
    Moving files in `lib` needed by production app into `app` is apparently
    the recommended way to do things now.

--- config/application.rb
+++ config/application.rb
@@ -11,10 +11,6 @@ module AlcesFlightCenter
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    # Autoload files from `lib`.
-    config.autoload_paths += %W(#{config.root}/lib)
-    config.autoload_paths += Dir["#{config.root}/lib/**/"]
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
