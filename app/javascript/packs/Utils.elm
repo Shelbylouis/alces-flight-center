d6e5874979c6f1983a3bffdd30ef58dc8f713ef3
Bob Whitelock <bob.whitelock1@gmail.com>
2017-10-25 19:41:44 +0100

    Extract function for creating SelectList from List
    Need to do this outside of existing decoder definition.

--- app/javascript/packs/Utils.elm
+++ app/javascript/packs/Utils.elm
@@ -14,17 +14,26 @@ selectListDecoder itemDecoder =
     let
         createSelectList =
             \list ->
-                let
-                    ( maybeHead, maybeTail ) =
-                        ( List.head list, List.tail list )
-                in
-                Maybe.map2
-                    (\head ->
-                        \tail ->
-                            D.succeed (SelectList.fromLists [] head tail)
-                    )
-                    maybeHead
-                    maybeTail
-                    |> Maybe.withDefault (D.fail "expected list with more than one element")
+                case selectListFromList list of
+                    Just selectList ->
+                        D.succeed selectList
+
+                    Nothing ->
+                        D.fail "expected list with more than one element"
     in
     D.list itemDecoder |> D.andThen createSelectList
+
+
+selectListFromList : List a -> Maybe (SelectList a)
+selectListFromList list =
+    let
+        ( maybeHead, maybeTail ) =
+            ( List.head list, List.tail list )
+    in
+    Maybe.map2
+        (\head ->
+            \tail ->
+                SelectList.fromLists [] head tail
+        )
+        maybeHead
+        maybeTail
