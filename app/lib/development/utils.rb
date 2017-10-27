3a652f8105efd3fa544fa00473b2204a1fd91d0a
Bob Whitelock <bob.whitelock1@gmail.com>
2017-10-27 17:59:57 +0100

    Create fixture document with full folder names
    Include the full path to each document file, relative to the document
    fixtures directory, in uploaded Cluster document fixtures, so can test
    files within 'folders' are handled correctly.

--- app/lib/development/utils.rb
+++ app/lib/development/utils.rb
@@ -3,12 +3,22 @@
 # needed then they should be moved elsewhere.
 module Development
   module Utils
+    DOCUMENT_FIXTURES_PATH = Pathname.new('fixtures/documents')
+
     class << self
       def upload_document_fixtures_for(cluster)
+        # Could delete existing files under Cluster path - would ensure only
+        # fixture files exist for Cluster, but would be destructive to existing
+        # data.
         s3 = Aws::S3::Resource.new(region: ENV.fetch('AWS_REGION'))
         bucket = s3.bucket(ENV.fetch('AWS_DOCUMENTS_BUCKET'))
-        Pathname.glob('fixtures/documents/*').each do |document|
-          document_object_path = File.join(cluster.documents_path, document.basename)
+
+        Pathname.glob("#{DOCUMENT_FIXTURES_PATH}/**/*").each do |document|
+          next unless document.file?
+          document_object_path = File.join(
+            cluster.documents_path,
+            document.relative_path_from(DOCUMENT_FIXTURES_PATH).to_s
+          )
           document_object = bucket.object(document_object_path)
           document_object.upload_file(document)
         end
