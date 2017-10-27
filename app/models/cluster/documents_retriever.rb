be4fc31d44440ad218f6ce09a9d1e026add3bd32
Bob Whitelock <bob.whitelock1@gmail.com>
2017-10-27 17:03:15 +0100

    Extract DocumentsRetriever from Cluster

---
+++ app/models/cluster/documents_retriever.rb
@@ -0,0 +1,44 @@
+
+module Cluster::DocumentsRetriever
+  Document = Struct.new(:name, :url)
+
+  class << self
+    ACCESS_KEY_ID = ENV.fetch('AWS_ACCESS_KEY_ID')
+    SECRET_ACCESS_KEY = ENV.fetch('AWS_SECRET_ACCESS_KEY')
+    BUCKET = ENV.fetch('AWS_DOCUMENTS_BUCKET')
+    REGION = ENV.fetch('AWS_REGION')
+
+    def retrieve(documents_path)
+      objects_under_path(documents_path).map do |object|
+        name = File.basename(object.key)
+        url = presigned_url_for(object)
+        Document.new(name, url)
+      end
+    end
+
+    private
+
+    def objects_under_path(path)
+      s3 = Aws::S3::Resource.new(
+        access_key_id: ACCESS_KEY_ID,
+        secret_access_key: SECRET_ACCESS_KEY,
+        region: REGION
+      )
+      response = s3.client.list_objects(
+        bucket: BUCKET,
+        prefix: path
+      )
+      response.contents
+    end
+
+    def presigned_url_for(object)
+      presigner = Aws::S3::Presigner.new
+      presigner.presigned_url(
+        :get_object,
+        bucket: BUCKET,
+        key: object.key,
+        expires_in: 60.minutes.to_i
+      )
+    end
+  end
+end
