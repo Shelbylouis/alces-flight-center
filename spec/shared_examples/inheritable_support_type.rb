364c33d132eae6c3fa634f89d4351d67d0cbaa40
Bob Whitelock <bob.whitelock1@gmail.com>
2017-10-30 14:37:20 +0000

    Make inheritable_support_type generic
    Rather than being dependent on Component, this now creates and tests an
    instance of whatever class is being described where it is included.

--- spec/shared_examples/inheritable_support_type.rb
+++ spec/shared_examples/inheritable_support_type.rb
@@ -1,18 +1,30 @@
 
 RSpec.shared_examples 'inheritable_support_type' do
+  let :class_factory_identifier do
+    SpecUtils.class_factory_identifier(described_class)
+  end
+
   describe '#support_type' do
     let :cluster { create(:cluster, support_type: 'advice') }
 
     it "returns cluster.support_type when set to 'inherit'" do
-      component = create(:component, support_type: 'inherit', cluster: cluster)
+      instance = create(
+        class_factory_identifier,
+        support_type: 'inherit',
+        cluster: cluster
+      )
 
-      expect(component.support_type).to eq('advice')
+      expect(instance.support_type).to eq('advice')
     end
 
     it 'returns own support_type otherwise' do
-      component = create(:component, support_type: 'managed', cluster: cluster)
+      instance = create(
+        class_factory_identifier,
+        support_type: 'managed',
+        cluster: cluster
+      )
 
-      expect(component.support_type).to eq('managed')
+      expect(instance.support_type).to eq('managed')
     end
   end
 end
