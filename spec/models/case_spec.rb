c367ef62bec07ac0d9ef757e422b109484e16e11
Bob Whitelock <bob.whitelock1@gmail.com>
2017-11-01 11:26:37 +0000

    Extract shared examples for Case-Component validations
    We want the same validations to occur for the Case-Service relationship
    as the Case-Component one. Extracting and making generic the
    Case-Component relationship validation tests is the first step towards
    this.

--- spec/models/case_spec.rb
+++ spec/models/case_spec.rb
@@ -1,23 +1,140 @@
 require 'rails_helper'
 
+# Shared examples for a Case-part relationship, where a part:
+# - is a class with a factory identified by `part_name`;
+# - has a `support_type`;
+# - has a `cluster`;
+# - a Case may be required or forbidden from having a relationship with a part,
+# depending on the `support_type` of both.
+RSpec.shared_examples 'associated Cluster part validation' do |part_name|
+  context "when issue requires #{part_name}" do
+    before :each do
+      requires_part_name = "requires_#{part_name}".to_sym
+      issue_attributes.merge!(requires_part_name => true)
+    end
+
+    let :managed_part_name { "managed_#{part_name}" }
+    let :advice_part_name { "advice_#{part_name}" }
+
+    # Explicitly specify advice cluster to ensure only relying on part
+    # `support_type`, in tests where this is relevant.
+    let :cluster { create(:advice_cluster) }
+
+    context "when not associated with #{part_name}" do
+      it 'should be invalid' do
+        expect(subject).not_to be_valid
+        expect(subject.errors.messages).to include(part_name => [/requires a #{part_name}/])
+      end
+    end
+
+    context "when associated with #{part_name} that is part of associated cluster" do
+      let :part { create(part_name, cluster: cluster) }
+      it { is_expected.to be_valid }
+    end
+
+    context "when associated with #{part_name} that is not part of associated cluster" do
+      let :part { create(part_name, cluster: create(:cluster)) }
+      it 'should be invalid' do
+        expect(subject).not_to be_valid
+        expect(subject.errors.messages).to include(part_name => [/not part of given cluster/])
+      end
+    end
+
+    context 'when `managed` issue' do
+      before :each do
+        issue_attributes.merge!(support_type: :managed)
+      end
+
+      context "with `managed` #{part_name}" do
+        let :part { create(managed_part_name, cluster: cluster) }
+        it { is_expected.to be_valid }
+      end
+
+      context "with `advice` #{part_name}" do
+        let :part { create(advice_part_name, cluster: cluster) }
+        it 'should be invalid' do
+          expect(subject).not_to be_valid
+          expect(subject.errors.messages).to include(
+            issue: [/is only available for fully managed #{part_name.to_s.pluralize}, but given #{part_name} is self-managed/]
+          )
+        end
+      end
+    end
+
+    context 'when `advice` issue' do
+      before :each do
+        issue_attributes.merge!(support_type: :advice)
+      end
+
+      context "with `managed` #{part_name}" do
+        let :part { create(managed_part_name, cluster: cluster) }
+        it { is_expected.to be_valid }
+      end
+
+      context "with `advice` #{part_name}" do
+        let :part { create(advice_part_name, cluster: cluster) }
+        it { is_expected.to be_valid }
+      end
+    end
+
+    context 'when `advice-only` issue' do
+      before :each do
+        issue_attributes.merge!(support_type: 'advice-only')
+      end
+
+      context "with `managed` #{part_name}" do
+        let :part { create(managed_part_name, cluster: cluster) }
+        it 'should be invalid' do
+          expect(subject).not_to be_valid
+          expect(subject.errors.messages).to include(
+            issue: [/is only available for self-managed #{part_name.to_s.pluralize}, but given #{part_name} is fully managed/]
+          )
+        end
+      end
+
+      context "with `advice` #{part_name}" do
+        let :part { create(advice_part_name, cluster: cluster) }
+        it { is_expected.to be_valid }
+      end
+    end
+  end
+end
+
+
 RSpec.describe Case, type: :model do
   describe '#valid?' do
     subject do
-      build(
-        :case,
+      attributes = {
         issue: issue,
         cluster: cluster,
         component: component
-      )
+      }
+
+      # If `part` is set, appropriately add Case attribute depending on `part`
+      # class.
+      if part
+        case part
+        when Component
+          attributes[:component] = part
+        else
+          raise "Unknown part class: #{part.class}"
+        end
+      end
+
+      build(:case, **attributes)
     end
+
     let :cluster { create(:cluster) }
     let :component { nil }
+    let :part { nil }
 
     let :issue { create(:issue, issue_attributes) }
 
     # Specify advice issue to avoid managed issue-related validations failing,
     # unless explicitly testing these.
     let :issue_attributes { attributes_for(:advice_issue) }
+
+    include_examples 'associated Cluster part validation', :component
 
     context 'when issue does not require component' do
       before :each do
@@ -91,94 +208,6 @@ RSpec.describe Case, type: :model do
 
         context 'with `advice` cluster' do
           let :cluster { create(:advice_cluster) }
-          it { is_expected.to be_valid }
-        end
-      end
-    end
-
-    context 'when issue requires component' do
-      before :each do
-        issue_attributes.merge!(requires_component: true)
-      end
-
-      # Explicitly specify advice cluster to ensure only relying on component
-      # `support_type`, in tests where this is relevant.
-      let :cluster { create(:advice_cluster) }
-
-      context 'when not associated with component' do
-        it 'should be invalid' do
-          expect(subject).not_to be_valid
-          expect(subject.errors.messages).to include(component: [/requires a component/])
-        end
-      end
-
-      context 'when associated with component that is part of associated cluster' do
-        let :component { create(:component, cluster: cluster) }
-        it { is_expected.to be_valid }
-      end
-
-      context 'when associated with component that is not part of associated cluster' do
-        let :component { create(:component, cluster: create(:cluster)) }
-        it 'should be invalid' do
-          expect(subject).not_to be_valid
-          expect(subject.errors.messages).to include(component: [/not part of given cluster/])
-        end
-      end
-
-      context 'when `managed` issue' do
-        before :each do
-          issue_attributes.merge!(support_type: :managed)
-        end
-
-        context 'with `managed` component' do
-          let :component { create(:managed_component, cluster: cluster) }
-          it { is_expected.to be_valid }
-        end
-
-        context 'with `advice` component' do
-          let :component { create(:advice_component, cluster: cluster) }
-          it 'should be invalid' do
-            expect(subject).not_to be_valid
-            expect(subject.errors.messages).to include(
-              issue: [/is only available for fully managed components, but given component is self-managed/]
-            )
-          end
-        end
-      end
-
-      context 'when `advice` issue' do
-        before :each do
-          issue_attributes.merge!(support_type: :advice)
-        end
-
-        context 'with `managed` component' do
-          let :component { create(:managed_component, cluster: cluster) }
-          it { is_expected.to be_valid }
-        end
-
-        context 'with `advice` component' do
-          let :component { create(:advice_component, cluster: cluster) }
-          it { is_expected.to be_valid }
-        end
-      end
-
-      context 'when `advice-only` issue' do
-        before :each do
-          issue_attributes.merge!(support_type: 'advice-only')
-        end
-
-        context 'with `managed` component' do
-          let :component { create(:managed_component, cluster: cluster) }
-          it 'should be invalid' do
-            expect(subject).not_to be_valid
-            expect(subject.errors.messages).to include(
-              issue: [/is only available for self-managed components, but given component is fully managed/]
-            )
-          end
-        end
-
-        context 'with `advice` component' do
-          let :component { create(:advice_component, cluster: cluster) }
           it { is_expected.to be_valid }
         end
       end
