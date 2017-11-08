874bad66d81231b7163251f65124a3c5919c9afe
Bob Whitelock <bob.whitelock1@gmail.com>
2017-11-08 18:08:16 +0000

    Migrate data to have multiple consultancy issues
    We want to be able to allow requesting custom consultancy for a Cluster
    as a whole, or for a particular Component or Service in a Cluster. This
    migration adapts the existing consultancy Issue and adds 2 new ones to
    support each of these cases.

---
+++ db/data_migrations/20171108172029_add_cluster_part_consultancy_issues.rb
@@ -0,0 +1,32 @@
+class AddClusterPartConsultancyIssues < ActiveRecord::DataMigration
+  def up
+    category = CaseCategory.find_by_name('Consultancy')
+
+    existing_issue = category.issues.first
+    existing_issue.name = consultancy_issue_text('cluster')
+    existing_issue.save!
+
+    new_issue_attributes = {
+      support_type: 'advice',
+      details_template: existing_issue.details_template
+    }
+
+    category.issues.create!(
+      **new_issue_attributes,
+      name: consultancy_issue_text('component'),
+      requires_component: true
+    )
+
+    category.issues.create!(
+      **new_issue_attributes,
+      name: consultancy_issue_text('service'),
+      requires_service: true
+    )
+  end
+
+  private
+
+  def consultancy_issue_text(model_name)
+    "Request custom consultancy for #{model_name}"
+  end
+end
