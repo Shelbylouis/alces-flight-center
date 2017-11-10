921914722c831eaf67473c7118f84f483fa631cc
Bob Whitelock <bob.whitelock1@gmail.com>
2017-11-10 17:23:40 +0000

    Show Service description, if present
    If a Service has a description available (via its associated
    ServiceType), then display this on the Service dashboard.

--- app/models/service.rb
+++ app/models/service.rb
@@ -7,6 +7,8 @@ class Service < ApplicationRecord
   validates :name, presence: true
   validates :support_type, inclusion: { in: SUPPORT_TYPES }, presence: true
 
+  delegate :description, to: :service_type
+
   # Automatically picked up by rails_admin so only these options displayed when
   # selecting support type.
   def support_type_enum
