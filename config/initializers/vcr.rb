de8e435e1b8194254cf0c19e2f5f932609cab4c6
Bob Whitelock <bob.whitelock1@gmail.com>
2017-11-13 12:31:20 +0000

    Log VCR debugging output

--- config/initializers/vcr.rb
+++ config/initializers/vcr.rb
@@ -1,4 +1,7 @@
 
 VCR.configure do |c|
   c.default_cassette_options = { :re_record_interval => 7.days }
+
+  # Log most recent debug output from VCR here.
+  c.debug_logger = File.open('log/vcr.log', 'w')
 end
