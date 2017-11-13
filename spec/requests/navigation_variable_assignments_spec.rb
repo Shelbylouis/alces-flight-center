5c2433ebf43b8005c52c60aaa9e3c96118ddf110
Bob Whitelock <bob.whitelock1@gmail.com>
2017-11-13 14:58:37 +0000

    DRY up contact/admin nav var assignment tests
    Once we are within the context for a particular Cluster, the assignment
    of navigation variables should function the same whether the current
    user is a site contact or Alces admin; therefore pull out a shared
    example for these tests to indicate this and remove this duplication.

--- spec/requests/navigation_variable_assignments_spec.rb
+++ spec/requests/navigation_variable_assignments_spec.rb
@@ -19,6 +19,50 @@ RSpec.describe 'Navigation variable assignments', type: :request do
     end
   end
 
+  RSpec.shared_examples 'cluster and part variable assignment' do
+    describe "get '/cluster/*'" do
+      before :each do
+        get cluster_path(cluster.id, as: user)
+      end
+
+      it 'assigns correct navigation variables' do
+        assigns_navigation_variables(
+          site: site,
+          cluster: cluster,
+          cluster_part: nil
+        )
+      end
+    end
+
+    describe "get /components/*" do
+      before :each do
+        get component_path(component.id, as: user)
+      end
+
+      it 'assigns correct navigation variables' do
+        assigns_navigation_variables(
+          site: site,
+          cluster: cluster,
+          cluster_part: component
+        )
+      end
+    end
+
+    describe "get /services/*" do
+      before :each do
+        get service_path(service.id, as: user)
+      end
+
+      it 'assigns correct navigation variables' do
+        assigns_navigation_variables(
+          site: site,
+          cluster: cluster,
+          cluster_part: service
+        )
+      end
+    end
+  end
+
   context 'when no user' do
     describe "get '/'" do
       before :each do
@@ -36,9 +80,13 @@ RSpec.describe 'Navigation variable assignments', type: :request do
   end
 
   context 'when user is site contact' do
+    let :user { contact }
+
+    include_examples 'cluster and part variable assignment'
+
     describe "get '/'" do
       before :each do
-        get root_path(as: contact)
+        get root_path(as: user)
       end
 
       it 'assigns correct navigation variables' do
@@ -49,55 +97,16 @@ RSpec.describe 'Navigation variable assignments', type: :request do
         )
       end
     end
-
-    describe "get '/cluster/*'" do
-      before :each do
-        get cluster_path(cluster.id, as: contact)
-      end
-
-      it 'assigns correct navigation variables' do
-        assigns_navigation_variables(
-          site: site,
-          cluster: cluster,
-          cluster_part: nil
-        )
-      end
-
-    end
-
-    describe "get /components/*" do
-      before :each do
-        get component_path(component.id, as: contact)
-      end
-
-      it 'assigns correct navigation variables' do
-        assigns_navigation_variables(
-          site: site,
-          cluster: cluster,
-          cluster_part: component
-        )
-      end
-    end
-
-    describe "get /services/*" do
-      before :each do
-        get service_path(service.id, as: contact)
-      end
-
-      it 'assigns correct navigation variables' do
-        assigns_navigation_variables(
-          site: site,
-          cluster: cluster,
-          cluster_part: service
-        )
-      end
-    end
   end
 
   context 'when user is admin' do
+    let :user { admin }
+
+    include_examples 'cluster and part variable assignment'
+
     describe "get '/'" do
       before :each do
-        get root_path(as: admin)
+        get root_path(as: user)
       end
 
       it 'assigns correct navigation variables' do
@@ -111,7 +120,7 @@ RSpec.describe 'Navigation variable assignments', type: :request do
 
     describe "get '/sites/*'" do
       before :each do
-        get site_path(site.id, as: admin)
+        get site_path(site.id, as: user)
       end
 
       it 'assigns correct navigation variables' do
@@ -119,48 +128,6 @@ RSpec.describe 'Navigation variable assignments', type: :request do
           site: site,
           cluster: nil,
           cluster_part: nil
-        )
-      end
-    end
-
-    describe "get '/cluster/*'" do
-      before :each do
-        get cluster_path(cluster.id, as: admin)
-      end
-
-      it 'assigns correct navigation variables' do
-        assigns_navigation_variables(
-          site: site,
-          cluster: cluster,
-          cluster_part: nil
-        )
-      end
-    end
-
-    describe "get /components/*" do
-      before :each do
-        get component_path(component.id, as: admin)
-      end
-
-      it 'assigns correct navigation variables' do
-        assigns_navigation_variables(
-          site: site,
-          cluster: cluster,
-          cluster_part: component
-        )
-      end
-    end
-
-    describe "get /services/*" do
-      before :each do
-        get service_path(service.id, as: admin)
-      end
-
-      it 'assigns correct navigation variables' do
-        assigns_navigation_variables(
-          site: site,
-          cluster: cluster,
-          cluster_part: service
         )
       end
     end
