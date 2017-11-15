ec80bceab2cb36d9f731bf87c1f24da71ebd3f06
Bob Whitelock <bob.whitelock1@gmail.com>
2017-11-15 17:30:29 +0000

    Add charging info field to Clusters

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171115165027) do
+ActiveRecord::Schema.define(version: 20171115172826) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -86,6 +86,7 @@ ActiveRecord::Schema.define(version: 20171115165027) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "canonical_name"
+    t.string "charging_info"
     t.index ["site_id"], name: "index_clusters_on_site_id"
   end
 
