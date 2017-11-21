53fbe263540be3c2812c359e0a8551cd13371dbe
Bob Whitelock <bob.whitelock1@gmail.com>
2017-11-21 13:13:02 +0000

    Only show maintenance button when allowed
    Only Cases which require a Component can be brought under maintenance,
    so only show the button to do this in this case.

--- spec/features/maintenance_windows_spec.rb
+++ spec/features/maintenance_windows_spec.rb
@@ -4,25 +4,42 @@ RSpec.feature "Maintenance windows", type: :feature do
   context 'when user is an admin' do
     let :user { create(:admin) }
 
-    it 'can switch a Case to/from under maintenance' do
-      support_case = create(:case)
+    let :start_link_path do
+      start_maintenance_window_case_path(support_case.id)
+    end
+    let :end_link_path do
+      end_maintenance_window_case_path(support_case.id)
+    end
 
-      visit site_cases_path(support_case.site, as: user)
+    context 'when Case has no Component' do
+      let :support_case { create(:case) }
 
-      start_link_path = start_maintenance_window_case_path(support_case.id)
-      end_link_path = end_maintenance_window_case_path(support_case.id)
+      it 'does not show start/end maintenance links' do
+        visit site_cases_path(support_case.site, as: user)
 
-      start_link = page.find_link(href: start_link_path)
-      expect(start_link).to have_css('.fa-wrench.interactive-icon')
-      start_link.click
-      expect(support_case).to be_under_maintenance
-      expect(page).not_to have_link(href: start_link_path)
+        expect(page).not_to have_link(href: start_link_path)
+        expect(page).not_to have_link(href: end_link_path)
+      end
+    end
 
-      end_link = page.find_link(href: end_link_path)
-      expect(end_link).to have_css('.fa-heartbeat.interactive-icon')
-      end_link.click
-      expect(support_case).not_to be_under_maintenance
-      expect(page).not_to have_link(href: end_link_path)
+    context 'when Case has Component' do
+      let :support_case { create(:case_with_component) }
+
+      it 'can switch a Case to/from under maintenance' do
+        visit site_cases_path(support_case.site, as: user)
+
+        start_link = page.find_link(href: start_link_path)
+        expect(start_link).to have_css('.fa-wrench.interactive-icon')
+        start_link.click
+        expect(support_case).to be_under_maintenance
+        expect(page).not_to have_link(href: start_link_path)
+
+        end_link = page.find_link(href: end_link_path)
+        expect(end_link).to have_css('.fa-heartbeat.interactive-icon')
+        end_link.click
+        expect(support_case).not_to be_under_maintenance
+        expect(page).not_to have_link(href: end_link_path)
+      end
     end
   end
 end
