59ca6c6b628c8257e4846349ae02e42245aa31e8
Bob Whitelock <bob.whitelock1@gmail.com>
2017-11-23 14:44:48 +0000

    Remove unnecessary context block
    No longer needed now we have determined that we want to be able to
    create MaintenanceWindows for any Case.

--- spec/features/maintenance_windows_spec.rb
+++ spec/features/maintenance_windows_spec.rb
@@ -4,6 +4,8 @@ RSpec.feature "Maintenance windows", type: :feature do
   context 'when user is an admin' do
     let :user { create(:admin) }
 
+    let :support_case { create(:case) }
+
     let :start_link_path do
       start_maintenance_window_case_path(support_case.id)
     end
@@ -11,24 +13,20 @@ RSpec.feature "Maintenance windows", type: :feature do
       end_maintenance_window_case_path(support_case.id)
     end
 
-    context 'when Case has Component' do
-      let :support_case { create(:case_with_component) }
+    it 'can switch a Case to/from under maintenance' do
+      visit site_cases_path(support_case.site, as: user)
 
-      it 'can switch a Case to/from under maintenance' do
-        visit site_cases_path(support_case.site, as: user)
+      start_link = page.find_link(href: start_link_path)
+      expect(start_link).to have_css('.fa-wrench.interactive-icon')
+      start_link.click
+      expect(support_case).to be_under_maintenance
+      expect(page).not_to have_link(href: start_link_path)
 
-        start_link = page.find_link(href: start_link_path)
-        expect(start_link).to have_css('.fa-wrench.interactive-icon')
-        start_link.click
-        expect(support_case).to be_under_maintenance
-        expect(page).not_to have_link(href: start_link_path)
-
-        end_link = page.find_link(href: end_link_path)
-        expect(end_link).to have_css('.fa-heartbeat.interactive-icon')
-        end_link.click
-        expect(support_case).not_to be_under_maintenance
-        expect(page).not_to have_link(href: end_link_path)
-      end
+      end_link = page.find_link(href: end_link_path)
+      expect(end_link).to have_css('.fa-heartbeat.interactive-icon')
+      end_link.click
+      expect(support_case).not_to be_under_maintenance
+      expect(page).not_to have_link(href: end_link_path)
     end
   end
 end
