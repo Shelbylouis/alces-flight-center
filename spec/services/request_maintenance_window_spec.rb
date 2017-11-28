29fb32dd090512aabdfc9bab4b00d8e9dfed199c
Bob Whitelock <bob.whitelock1@gmail.com>
2017-11-28 13:10:35 +0000

    Extrack subject block to DRY up tests

--- spec/services/request_maintenance_window_spec.rb
+++ spec/services/request_maintenance_window_spec.rb
@@ -8,81 +8,67 @@ RSpec.describe RequestMaintenanceWindow do
   let :user { create(:user) }
   let :user_name { user.name }
   let :support_case { create(:case) }
+  let :associated_model { nil }
 
-  it 'creates new MaintenanceWindow and adds RT correspondence' do
-    associated_model = create(:component)
-    cluster = associated_model.cluster
-
-    expect(Case.request_tracker).to receive(
-      :add_ticket_correspondence
-    ).with(
-      id: support_case.rt_ticket_id,
-      text: /requested.*#{associated_model.name}.*by #{user_name}.*must be confirmed.*#{cluster_url(cluster)}/
-    )
-
-    maintenance_window = RequestMaintenanceWindow.new(
+  subject do
+    RequestMaintenanceWindow.new(
       support_case: support_case,
       user: user,
       associated_model: associated_model
     ).run
+  end
 
-    expect(maintenance_window.ended_at).to be nil
-    expect(maintenance_window.user).to eq user
-    expect(maintenance_window.case).to eq support_case
+  describe 'main behaviour' do
+    let :associated_model { create(:component) }
+
+    it 'creates new MaintenanceWindow and adds RT correspondence' do
+      cluster = associated_model.cluster
+
+      expect(Case.request_tracker).to receive(
+        :add_ticket_correspondence
+      ).with(
+        id: support_case.rt_ticket_id,
+        text: /requested.*#{associated_model.name}.*by #{user_name}.*must be confirmed.*#{cluster_url(cluster)}/
+      )
+
+      expect(subject.ended_at).to be nil
+      expect(subject.user).to eq user
+      expect(subject.case).to eq support_case
+    end
   end
 
   context 'when given no associated model' do
     let :support_case { create(:case_with_component) }
 
     it "is associated with Case's associated model" do
-      maintenance_window = RequestMaintenanceWindow.new(
-        support_case: support_case,
-        user: user
-      ).run
-
-      expect(maintenance_window.associated_model).to eq support_case.component
+      expect(subject.associated_model).to eq support_case.component
     end
   end
 
   context 'when given associated Component' do
     let :component { create(:component) }
+    let :associated_model { component }
 
     it 'is associated with Component' do
-      maintenance_window = RequestMaintenanceWindow.new(
-        support_case: support_case,
-        user: user,
-        associated_model: component
-      ).run
-
-      expect(maintenance_window.associated_model).to eq component
+      expect(subject.associated_model).to eq component
     end
   end
 
   context 'when given associated Cluster' do
     let :cluster { create(:cluster) }
+    let :associated_model { cluster }
 
     it 'is associated with Cluster'  do
-      maintenance_window = RequestMaintenanceWindow.new(
-        support_case: support_case,
-        user: user,
-        associated_model: cluster
-      ).run
-
-      expect(maintenance_window.associated_model).to eq cluster
+      expect(subject.associated_model).to eq cluster
     end
   end
 
   context 'when given associated Service' do
     let :service { create(:service) }
+    let :associated_model { service }
 
     it 'is associated with Service' do
-      maintenance_window = RequestMaintenanceWindow.new(
-        support_case: support_case,
-        user: user,
-        associated_model: service
-      ).run
-
-      expect(maintenance_window.associated_model).to eq service
+      expect(subject.associated_model).to eq service
     end
   end
 end
