ba5c5bd5e68d3ab12e860705d024c8665f35dae7
Bob Whitelock <bob.whitelock1@gmail.com>
2017-11-29 12:15:29 +0000

    Extract file for maintenance window factories

--- spec/factories.rb
+++ spec/factories.rb
@@ -80,24 +80,4 @@ FactoryBot.define do
     user { create(:admin) }
     amount 2
   end
-
-  factory :maintenance_window do
-    add_attribute(:case) { create(:case) } # Avoid conflict with case keyword.
-    user { create(:admin) }
-    created_at 7.days.ago
-
-    # This could also be a Cluster or Service; but one of these must be
-    # associated and is the item under maintenance.
-    component
-
-    factory :unconfirmed_maintenance_window {}
-
-    factory :confirmed_maintenance_window do
-      confirmed_by { create(:contact) }
-
-      factory :closed_maintenance_window do
-        ended_at 3.days.ago
-      end
-    end
-  end
 end
