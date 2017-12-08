23f91fee2b5349f2708efad41bbfc23d0b59435a
Bob Whitelock <bob.whitelock1@gmail.com>
2017-12-08 15:41:32 +0000

    Remove unused variables

--- app/controllers/consultancy_controller.rb
+++ app/controllers/consultancy_controller.rb
@@ -2,9 +2,6 @@ class ConsultancyController < ApplicationController
   def new
     @title = 'Request custom consultancy'
 
-    cluster_id = params[:cluster_id]
-    component_id = params[:component_id]
-    service_id = params[:service_id]
     @case = Case.new(
       cluster_id: params[:cluster_id],
       component_id: params[:component_id],
