b2d197ea1780189e7a2ef629ee450cccabd287fd
Bob Whitelock <bob.whitelock1@gmail.com>
2017-12-15 14:38:08 +0000

    Remove now unnecessary parts of Category module
    Following recent refactoring these functions are superfluous or have
    been moved and adapted elsewhere.

--- app/javascript/packs/Category.elm
+++ app/javascript/packs/Category.elm
@@ -5,27 +5,19 @@ module Category
           -- , availableForSelectedCluster
         , decoder
         , extractId
-        , filterByIssues
-        , isControlledByService
         , setSelectedIssue
         )
 
-import Cluster exposing (Cluster)
 import Issue exposing (Issue)
 import Json.Decode as D
-import Maybe.Extra
 import SelectList exposing (SelectList)
 import SelectList.Extra
-import Service exposing (Service)
-import ServiceType exposing (ServiceType)
-import Utils
 
 
 type alias Category =
     { id : Id
     , name : String
     , issues : SelectList Issue
-    , controllingServiceType : Maybe ServiceType
     }
 
 
@@ -35,11 +27,10 @@ type Id
 
 decoder : D.Decoder Category
 decoder =
-    D.map4 Category
+    D.map3 Category
         (D.field "id" D.int |> D.map Id)
         (D.field "name" D.string)
         (D.field "issues" (SelectList.Extra.orderedDecoder Issue.name Issue.decoder))
-        (D.field "controllingServiceType" (D.nullable ServiceType.decoder))
 
 
 
@@ -51,22 +42,6 @@ decoder =
 --     -- selection of the Category.
 --     SelectList.toList caseCategory.issues
 --         |> List.any (Issue.Utils.availableForSelectedCluster clusters)
-
-
-filterByIssues : SelectList Category -> (Issue -> Bool) -> Maybe (SelectList Category)
-filterByIssues caseCategories condition =
-    SelectList.map (withJustMatchingIssues condition) caseCategories
-        |> SelectList.toList
-        |> Maybe.Extra.values
-        |> SelectList.Extra.fromList
-
-
-withJustMatchingIssues : (Issue -> Bool) -> Category -> Maybe Category
-withJustMatchingIssues condition caseCategory =
-    SelectList.toList caseCategory.issues
-        |> List.filter condition
-        |> SelectList.Extra.fromList
-        |> Maybe.map (asIssuesIn caseCategory)
 
 
 extractId : Category -> Int
@@ -88,17 +63,3 @@ setSelectedIssue caseCategories issueId =
 asIssuesIn : Category -> SelectList Issue -> Category
 asIssuesIn caseCategory issues =
     { caseCategory | issues = issues }
-
-
-{-|
-
-    A Category is 'controlled by' a Service iff its
-    `controllingServiceType` is the same as the Service's ServiceType.
-
--}
-isControlledByService : Service -> Category -> Bool
-isControlledByService service caseCategory =
-    Maybe.map
-        (Utils.sameId service.serviceType.id)
-        caseCategory.controllingServiceType
-        |> Maybe.withDefault False
