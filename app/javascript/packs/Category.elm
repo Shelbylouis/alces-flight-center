c55e1f7908d77195c2fb48f90de87279d3b6da51
Bob Whitelock <bob.whitelock1@gmail.com>
2017-12-18 10:18:44 +0000

    Remove commented out function

--- app/javascript/packs/Category.elm
+++ app/javascript/packs/Category.elm
@@ -34,17 +34,6 @@ decoder =
         (D.field "issues" (SelectList.Extra.orderedDecoder Issue.name Issue.decoder))
 
 
-
--- XXX disabled for now; may adapt and add back later when add back Categorys.
--- availableForSelectedCluster : SelectList Cluster -> Category -> Bool
--- availableForSelectedCluster clusters category =
---     -- A Category should be available only if any Issue within it is
---     -- available for the selected Cluster, otherwise there is no point allowing
---     -- selection of the Category.
---     SelectList.toList category.issues
---         |> List.any (Issue.Utils.availableForSelectedCluster clusters)
-
-
 extractId : Category -> Int
 extractId category =
     case category.id of
