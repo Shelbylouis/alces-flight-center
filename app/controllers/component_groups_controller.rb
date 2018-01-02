f69bfec257fdf9cac8851a6a91ecbfe55897a9ab
william <william.mccumstie@outlook.com>
2018-01-02 17:06:46 +0000

    Display the asset records for a ComponentGroup
    Based off the code for a Component. Currently the code has been
    duplicated, this should be fixed in the future

--- app/controllers/component_groups_controller.rb
+++ app/controllers/component_groups_controller.rb
@@ -1,4 +1,6 @@
 class ComponentGroupsController < ApplicationController
+  decorates_assigned :component_group
+
   def show
     @component_group = ComponentGroup.find(params[:id])
   end
--- app/models/component_group.rb
+++ app/models/component_group.rb
@@ -19,6 +19,17 @@ class ComponentGroup < ApplicationRecord
     components.map(&:name)
   end
 
+  def asset_record
+    # Merge asset record layers to obtain hash for this Component of all
+    # asset record fields for this ComponentType; fields set in later layers
+    # will take precedence over those in earlier layers for the same
+    # definition.
+    @asset_record ||=
+      asset_record_layers.reduce({}, :merge).values.map do |field|
+        [field.name, field.value]
+      end.to_h
+  end
+
   private
 
   def create_needed_components_for_host_range
@@ -41,5 +52,33 @@ class ComponentGroup < ApplicationRecord
       nodeattr_output = `nodeattr -f #{file.path} -c #{genders_attr}`
       nodeattr_output.strip.split(',')
     end
+  end
+
+  def asset_record_layers
+    # Each entry is a hash of definition ID to asset record field.
+    [
+      empty_asset_record_fields,
+      group_asset_record_fields,
+    ]
+  end
+
+  def empty_asset_record_fields
+    component_type.asset_record_field_definitions.map do |definition|
+      [
+        definition.id,
+        # Placeholder empty AssetRecordField.
+        AssetRecordField.new(definition: definition, value: ''),
+      ]
+    end.to_h
+  end
+
+  def group_asset_record_fields
+    extract_asset_record_fields(self)
+  end
+
+  def extract_asset_record_fields(model)
+    model.asset_record_fields.map do |field|
+      [field.definition.id, field]
+    end.to_h
   end
 end
