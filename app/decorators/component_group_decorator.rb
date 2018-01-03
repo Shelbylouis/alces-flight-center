8c28ad8b38a1458a6ca794f80b454e343965733d
william <william.mccumstie@outlook.com>
2018-01-03 13:05:04 +0000

    Create link to the component groups from cluster
    The code to generate the link text has been moved to a decorator for
    clarity.

--- app/decorators/component_group_decorator.rb
+++ app/decorators/component_group_decorator.rb
@@ -1,4 +1,10 @@
 class ComponentGroupDecorator < ApplicationDecorator
+  include ActionView::Helpers::TextHelper
+
   delegate_all
   decorates_association :components
+
+  def link_text
+    "#{name} — #{pluralize(components.length, component_type.name)}"
+  end
 end
