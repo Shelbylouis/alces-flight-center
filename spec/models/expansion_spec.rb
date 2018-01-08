f89e934282edebe62a7b449f7f541d13266ce455
william <william.mccumstie@outlook.com>
2018-01-08 19:16:24 +0000

    Prevent a base Expansion from being created
    This was already done by the db, however this commit adds it into
    application logic. The spec has also been updated to reflect the
    change.

--- spec/models/expansion_spec.rb
+++ spec/models/expansion_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Expansion, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # The type field specifies which flavour of Expansion is required
+  # However it is left blank in the base Expansion model
+  it 'a base Expansion can not be created' do
+    expect(Expansion.create.errors.messages).to include(:type)
+  end
 end
