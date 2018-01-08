9a2a1e032a5625e02e7fc41b5819dc958f4debb5
william <william.mccumstie@outlook.com>
2018-01-08 18:42:28 +0000

    Enforce DefaultExpansion relationsip in the model
    As the expansion table allows null values for the ComponentMake and
    Component relationship, they need to be enforced in application logic.
    
    In the case of `DefaultExpansion`, it must have a relationship to
    `ComponentMake`. It does not have a `component` method, however it
    will still respond to `component_id` which must be (null/ absent).

--- app/models/default_expansion.rb
+++ app/models/default_expansion.rb
@@ -1,6 +1,9 @@
 class DefaultExpansion < Expansion
   belongs_to :component_make
 
+  validates :component_make, presence: true
+  validates :component_id, absence: true
+
   def self.globally_available?
     true
   end
