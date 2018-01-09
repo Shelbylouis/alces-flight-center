04cd7fed10f46ba46b3afeee03a6d9a1513a115c
william <william.mccumstie@outlook.com>
2018-01-09 20:32:48 +0000

    Switch Component to use HasAssetRecordField
    The code has been DRY'ed up and replaced with the Concern. The parent
    class assets are from ComponentGroup which will access the blank
    assets from ComponentType.
    
    There is a bug caused by the collection being an array instead of
    an ActiveRecord::...::Collection (of some description). This will
    be fixed in a later commit.

--- app/models/concerns/has_asset_record_fields.rb
+++ app/models/concerns/has_asset_record_fields.rb
@@ -2,6 +2,12 @@
 module HasAssetRecordFields
   extend ActiveSupport::Concern
 
+  # Method to be called from AdminConfig to format Component asset record for
+  # displaying to admins.
+  def asset_record_view
+    asset_record.to_json
+  end
+
   def asset_record
     @asset_record ||=
       asset_record_fields.map(&:value).map do |field|
