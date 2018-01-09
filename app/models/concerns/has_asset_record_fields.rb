2ac615b87c2ff0d5fdffb49bab822771106f9af6
william <william.mccumstie@outlook.com>
2018-01-09 18:51:53 +0000

    Merge the asset_record_fields with it's parent class
    Previous the asset_record_fields for a model where determined by what
    was stored in the asset_record_layers method. This means that the
    order has to be maintained in multiple modules and may get out of
    sync.
    
    Instead a new parent_asset_record_fields method has to be specified.
    This needs to be an Enumerable object of the parent assets. The
    new assets are then merged over the top of it similarly to before.
    
    By chaining multiple models together, the same affect can be achieved
    as before.

--- app/models/concerns/has_asset_record_fields.rb
+++ app/models/concerns/has_asset_record_fields.rb
@@ -3,9 +3,17 @@ module HasAssetRecordFields
   extend ActiveSupport::Concern
 
   def asset_record_fields
-    super
+    hashify_asset_record_fields(parent_asset_record_fields)
+      .merge(hashify_asset_record_fields(super))
+      .values
   end
-  
+
+  def hashify_asset_record_fields(records)
+    (records || []).map do |field|
+      [field.definition.id, field]
+    end.to_h
+  end
+
   def asset_record_layers
     # Each entry is a hash of definition ID to asset record field.
     [
@@ -18,6 +26,7 @@ module HasAssetRecordFields
     extract_asset_record_fields(self)
   end
 
+  # TODO: Remove this
   def extract_asset_record_fields(model)
     model.asset_record_fields.map do |field|
       [field.definition.id, field]
--- spec/models/concerns/has_asset_record_fields_spec.rb
+++ spec/models/concerns/has_asset_record_fields_spec.rb
@@ -1,21 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe HasAssetRecordFields, type: :model do
+  let :parent do
+    make_obj('parent-asset': 2, 'idx3-parent': 3)
+  end
+
   subject do
-    assets = [
-      double(AssetRecordField, value: 'my-asset')
-    ] 
+    make_obj(parent, 'my-asset': 1, 'idx3-child': 3)
+  end
+
+  def make_obj(parent = nil, **fields)
+    assets = fields.each_with_object([]) do |(msg, id), memo|
+      definition = { definition: double(id: id) }
+      d = double(AssetRecordField, **definition, value: msg.to_s)
+      memo.push(d)
+    end
     OpenStruct.new(
       name: 'Component-ish',
-      asset_record_fields: assets
+      asset_record_fields: assets,
+      parent_asset_record_fields: parent&.asset_record_fields
     ).tap { |x| x.extend(HasAssetRecordFields) }
   end
- 
+
   def asset_values(obj = subject)
     obj.asset_record_fields.map(&:value)
   end
 
   it 'includes the asset_record_fields for the current layer' do
     expect(asset_values).to include('my-asset')
+  end
+
+  it 'includes its parent assets' do
+    expect(asset_values).to include('parent-asset')
+  end
+
+  it 'child assets override their parents' do
+    expect(asset_values).to include('idx3-child')
+    expect(asset_values).not_to include('idx3-parent')
   end
 end
