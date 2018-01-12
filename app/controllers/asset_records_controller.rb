da6012bb7a6d99d4a91575d9b98ad4bb249787a4
william <william.mccumstie@outlook.com>
2018-01-12 17:38:38 +0000

    Error if multiple asset ids are sent with the request
    If for some reason multiple asset ids are sent in the request, an error
    should be raise as the request is invalid.
    
    Otherwise it is possible that one id will will likely win in some
    undefined manner.

--- app/controllers/asset_records_controller.rb
+++ app/controllers/asset_records_controller.rb
@@ -2,5 +2,24 @@ class AssetRecordsController < ApplicationController
   def edit
     @title = "Title"
     @subtitle = "Subtitle"
+    asset
+  end
+
+  private
+
+  VALID_ASSET_IDS = [:component_id, :component_group_id]
+
+  def asset
+    ensure_single_asset_only
+  end
+
+  def ensure_single_asset_only
+    (asset_params.keys & VALID_ASSET_IDS.map(&:to_s)).tap do |ids|
+      raise 'Can not determine asset' unless ids.length == 1
+    end
+  end
+
+  def asset_params
+    params.permit(*VALID_ASSET_IDS)
   end
 end
---
+++ spec/features/asset_record.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.feature 'Asset Record', type: :feature   do
+  let :admin { create(:admin) }
+  let :component { create(:component) }
+  let :component_group { create(:component_group) }
+
+  context 'with an invalid request' do
+    it 'errors as a non-admin user' do
+      expect do
+        visit edit_component_asset_record_path(component)
+      end.to raise_error(ActionController::RoutingError)
+    end
+
+    it 'errors if component_group is sent with a component request' do
+      expect do
+        visit edit_component_asset_record_path(
+          component, as: admin, component_group_id: component_group
+        )
+      end.to raise_error(RuntimeError)
+    end
+  end
+end
