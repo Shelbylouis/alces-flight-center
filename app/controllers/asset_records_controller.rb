3b26bacd712bd98567ca5e7f5eb65fe087b132a1
william <william.mccumstie@outlook.com>
2018-01-19 11:37:17 +0000

    Simplify code for determining the asset
    The `ApplicationController` already sets the component_group and
    component_part so they do not need to be manually determined here.
    
    Instead a simply 'or' can be used between them

--- app/controllers/asset_records_controller.rb
+++ app/controllers/asset_records_controller.rb
@@ -10,17 +10,7 @@ class AssetRecordsController < ApplicationController
 
   private
 
-  VALID_ASSET_IDS = [:component_id, :component_group_id]
-
   def asset
-    id_key = id_param.keys.first
-    id_key.to_s.chomp('_id').classify.constantize.find(id_param[id_key])
-  end
-
-  def id_param
-    id = (params.keys & VALID_ASSET_IDS.map(&:to_s)).tap do |keys|
-      raise 'Can not determine asset' unless keys.length == 1
-    end
-    params.permit(*id)
+    @cluster_part || @component_group
   end
 end
