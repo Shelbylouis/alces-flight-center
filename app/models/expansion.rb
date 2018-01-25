49f739c532e3b6246d9511d5809543cd95704490
william <william.mccumstie@outlook.com>
2018-01-25 13:29:25 +0000

    Constrain ports to be an integer >= 0

--- app/models/expansion.rb
+++ app/models/expansion.rb
@@ -1,4 +1,8 @@
 class Expansion < ApplicationRecord
   belongs_to :expansion_type
   validates :type, :slot, :ports, presence: true
+  validates :ports, numericality: {
+    greater_than_or_equal_to: 0,
+    only_integer: true
+  }
 end
