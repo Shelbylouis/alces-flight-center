13a352fe10003849551c2e1ca680e319567a64db
william <william.mccumstie@outlook.com>
2018-01-25 13:08:27 +0000

    Update the expansion record from form data
    Currently there is no validation on the submitted data. This will be
    added in a future commit.

--- app/controllers/component_expansions_controller.rb
+++ app/controllers/component_expansions_controller.rb
@@ -1,10 +1,28 @@
 class ComponentExpansionsController < ApplicationController
   def update
+    @cluster_part.component_expansions.each do |expansion|
+      update_expansion(expansion)
+    end
     redirect_to @cluster_part
   end
 
   def edit
     @title = "Edit Expansions"
     @subtitle = @cluster_part.name
+  end
+
+  private
+
+  def update_expansion(expansion)
+    expansion.update!(expansion_param(expansion))
+  end
+
+  def expansion_param(expansion)
+    id = expansion.id
+    raw_params = params.require([:"slot#{id}", :"ports#{id}"])
+    {
+      slot: raw_params[0],
+      ports: raw_params[1]
+    }
   end
 end
