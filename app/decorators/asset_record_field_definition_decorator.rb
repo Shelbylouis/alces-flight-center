c2ae8934aaa526970ff9bab76b13bfe8d8507d27
william <william.mccumstie@outlook.com>
2018-01-30 11:52:35 +0000

    Move form input code to ARFDefinitionDecorator
    This is the new home for this code, however some of it might still get
    broken out.
    
    Currently the asset needs to be passed in along with the value, in order
    to generate the field. This will be moved to AssetRecordFieldDecorator
    in the next commit.

--- app/decorators/asset_record_field_definition_decorator.rb
+++ app/decorators/asset_record_field_definition_decorator.rb
@@ -1,8 +1,32 @@
 
 class AssetRecordFieldDefinitionDecorator < ApplicationDecorator
-  delegate_all
+  READ_ONLY_MSG = <<-EOF
+    This field can only be set at the individual Component level
+  EOF
 
-  def form_input
+  def form_input(value, asset)
+    options = {
+      class: 'form-control',
+      readonly: readonly(asset),
+      placeholder: placeholder(asset)
+    }.tap do |opt|
+      if opt[:readonly]
+        opt[:style] = 'background-color:lightgray'
+        opt[:title] = READ_ONLY_MSG
+      end
+    end
+
+    h.text_field_tag(object.id, value, **options)
+  end
+
+  private
+
+  def readonly(asset)
+    (object.level == 'component') && !(asset.is_a? Component)
+  end
+
+  def placeholder(asset)
+    asset.find_parent_asset_record(object).value
   end
 end
 
