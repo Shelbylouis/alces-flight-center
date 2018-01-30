72e37893882ac2baa9f0374ccc048f8fcb8b5d1f
william <william.mccumstie@outlook.com>
2018-01-30 12:23:17 +0000

    Create AssetRecordFieldDecorator to contain shared code
    The options block is highly dependent on the ARField and current asset
    and thus should not be stored in the ARFDefDecorator which will be
    overridden with different types.
    
    Instead the options and value will be determined in a ARFieldDecorator
    and then passed onto the Definition.
    
    The current_asset still needs to be passed in as this can not be
    inferred from AssetRecordField.

--- app/decorators/asset_record_field_definition_decorator.rb
+++ app/decorators/asset_record_field_definition_decorator.rb
@@ -1,32 +1,7 @@
 
 class AssetRecordFieldDefinitionDecorator < ApplicationDecorator
-  READ_ONLY_MSG = <<-EOF
-    This field can only be set at the individual Component level
-  EOF
-
-  def form_input(value, asset)
-    options = {
-      class: 'form-control',
-      readonly: readonly(asset),
-      placeholder: placeholder(asset)
-    }.tap do |opt|
-      if opt[:readonly]
-        opt[:style] = 'background-color:lightgray'
-        opt[:title] = READ_ONLY_MSG
-      end
-    end
-
+  def form_input(value, **options)
     h.text_field_tag(object.id, value, **options)
-  end
-
-  private
-
-  def readonly(asset)
-    (object.level == 'component') && !(asset.is_a? Component)
-  end
-
-  def placeholder(asset)
-    asset.find_parent_asset_record(object).value
   end
 end
 
