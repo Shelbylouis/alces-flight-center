5b65021891979bfa897992a4efa8d60a7afce394
Bob Whitelock <bob.whitelock1@gmail.com>
2018-01-30 11:20:28 +0000

    Encode and decode default subject for Case form
    As the first step of supporting editing of created Case subjects.
    
    The default subject is currently the same as the Issue name, however
    this may later change and if so we want this logic to only live in one
    place (within the Issue class).

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -72,9 +72,16 @@ class Issue < ApplicationRecord
       id: id,
       name: name,
       detailsTemplate: details_template,
+      defaultSubject: default_subject,
       requiresComponent: requires_component,
       supportType: support_type,
       chargeable: chargeable
     }
+  end
+
+  private
+
+  def default_subject
+    name
   end
 end
