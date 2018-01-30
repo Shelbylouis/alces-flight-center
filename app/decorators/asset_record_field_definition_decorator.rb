180598345f7deb9333670a267f2826cc8e42c789
william <william.mccumstie@outlook.com>
2018-01-30 15:20:39 +0000

    Display a text area input for LongText type
    Now that STI isn't being used, the method to switch the input has been
    changed to a case.

--- app/decorators/asset_record_field_definition_decorator.rb
+++ app/decorators/asset_record_field_definition_decorator.rb
@@ -1,6 +1,22 @@
 
 class AssetRecordFieldDefinitionDecorator < ApplicationDecorator
   def form_input(value, **options)
+    case object.data_type
+    when 'long_text'
+      text_area(value, options)
+    else
+      text_field(value, options)
+    end
+  end
+
+  private
+
+  def text_area(value, options)
+    options[:style] = "#{options[:style]} height:100px"
+    h.text_area_tag(object.id, value, **options)
+  end
+
+  def text_field(value, options)
     h.text_field_tag(object.id, value, **options)
   end
 end
