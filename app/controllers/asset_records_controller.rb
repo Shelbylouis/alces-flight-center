16e28143b81e91bd98d11740d2a308753715bf57
william <william.mccumstie@outlook.com>
2018-02-01 18:06:33 +0000

    Flash success or error messages when updating
    If no errors have occurred then it redirects to the asset and flashes
    success.
    
    Failed records trigger a error message to be flashed as they are
    redirected back to the same page. Successful updates still occur.

--- app/controllers/asset_records_controller.rb
+++ app/controllers/asset_records_controller.rb
@@ -8,13 +8,24 @@ class AssetRecordsController < ApplicationController
     update_asset_record
     update_component_make
     if error_objects.empty?
+      flash[:success] = 'Successfully updated the asset record'
       redirect_to asset
     else
+      flash_errors
       redirect_back fallback_location: @asset
     end
   end
 
   private
+
+  def flash_errors
+    header = 'The following records failed to update:'
+    error_objects.each_with_object([header]) do |model, array|
+      msg = "#{model.definition.field_name}: #{model.errors.full_messages}"
+      array.push msg
+    end.join("\n")
+       .tap { |full_msg| flash[:error] = full_msg }
+  end
 
   def error_objects
     @error_objects ||= []
