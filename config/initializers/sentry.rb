90f84944b8059305642935d548fa15605602ed51
James Muscat <james.muscat@alces-software.com>
2018-02-07 18:53:57 +0000

    Make Sentry DSN configuration optional.
    If a DSN isn't set (e.g. for running in development) then Sentry won't
    report errors. This is probably what we want when running on dev
    machines.

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,7 +1,9 @@
 
-Raven.configure do |config|
-  config.dsn = ENV.fetch('SENTRY_DSN')
+if ENV.has_key?('SENTRY_DSN')
+  Raven.configure do |config|
+    config.dsn = ENV.fetch('SENTRY_DSN')
 
-  # Do not send any filtered parameters to Sentry.
-  config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
+    # Do not send any filtered parameters to Sentry.
+    config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
+  end
 end
