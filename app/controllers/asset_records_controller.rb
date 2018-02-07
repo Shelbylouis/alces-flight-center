b60e9464b86db79e80ddb0300328dee462b9c44a
william <william.mccumstie@outlook.com>
2018-02-07 12:21:47 +0000

    Streamline asset record update process
    Previously errors where being collected in a class object and then
    independently iterated over to flash the errors. This makes it hard to
    follow what exactly is being displayed.
    
    Instead the failed updates are found in the `update` method and directly
    passed into the `error_flash_models` method.
    
    Addresses:
    https://github.com/alces-software/alces-flight-center/pull/49#discussion_r165943336

--- app/controllers/asset_records_controller.rb
+++ app/controllers/asset_records_controller.rb
@@ -6,34 +6,24 @@ class AssetRecordsController < ApplicationController
 
   def update
     update_component_make if asset.is_a? ComponentGroup
-    update_asset_record
-    if error_objects.empty?
+    failed_updates = update_asset_record.reject(&:nil?)
+                                        .reject(&:valid?)
+    if failed_updates.empty?
       flash[:success] = 'Successfully updated the asset record'
       redirect_to asset
     else
-      flash_errors
+      header = 'The following records failed to update:'
+      error_flash_models(failed_updates, header) do |model|
+        "#{model.definition.field_name}: #{model.errors.full_messages}"
+      end
       redirect_back fallback_location: @asset
     end
   end
 
   private
 
-  def flash_errors
-    header = 'The following records failed to update:'
-    error_flash_models(error_objects, header) do |model|
-      "#{model.definition.field_name}: #{model.errors.full_messages}"
-    end
-  end
-
-  def error_objects
-    @error_objects ||= []
-  end
-
   def update_asset_record
     asset.update_asset_record(asset_record_field_params.to_h)
-         .reject(&:nil?)
-         .reject(&:valid?)
-         .tap { |errors| error_objects.concat errors }
   end
 
   def update_component_make
