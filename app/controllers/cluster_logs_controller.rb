af64d334f976bc5df85db8a7959d8fdb65de5a99
william <william.mccumstie@outlook.com>
2018-02-09 14:32:45 +0000

    Format form for creating new logs
    The details and submit fields have been added to the form. The engineer
    who is submitting the log can be inferred server side as the
    current_user.
    
    The form has been correctly formatted using bootstrap and labels.
    
    There was an error where the form was for a new `Case` not `ClusterLog`.
    This now has been fixed

--- app/controllers/cluster_logs_controller.rb
+++ app/controllers/cluster_logs_controller.rb
@@ -2,7 +2,7 @@ class ClusterLogsController < ApplicationController
   def index
     @title = 'Logs'
     @logs = @cluster.cluster_logs
-    @new_case = @cluster.cases.new
+    @new_log = @cluster.cluster_logs.new
     @cases = @cluster.cases.order(created_at: :desc)
   end
 
