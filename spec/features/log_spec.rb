ee46e1606017c5f0667983cc1de549f45ba23fa9
william <william.mccumstie@outlook.com>
2018-02-19 17:32:32 +0000

    Add cases for the component in the spec

--- spec/features/log_spec.rb
+++ spec/features/log_spec.rb
@@ -94,7 +94,14 @@ RSpec.feature Log, type: :feature do
 
   context 'when visiting the component log' do
     subject { cluster.components.first }
-    before :each { visit component_logs_path subject, as: engineer }
+    before :each do
+      visit component_logs_path subject, as: engineer
+      ['component_case1', 'component_case2'].each do |details|
+        create :case_requiring_component,
+               component: subject,
+               details: details
+      end
+    end
 
     include_examples 'shared log features'
   end
