57c57d1e836a7721677d0d7b5b9758e111671922
william <william.mccumstie@outlook.com>
2018-02-19 12:27:26 +0000

    Create a context for testing the cluster log
    Now that a log can be for a cluster and a component, the feature spec
    has been updated to include a context. This will make it easier to add
    the component specs in the future.
    
    The spec has also been updated to create some test components. The
    cluster log page should display a select box to display these
    components.
    
    A random :component and :case has also been added. These random models
    belong to a different cluster and thus should be ignored. They represent
    all other DB entries and should not affect the tests.

--- spec/features/log_spec.rb
+++ spec/features/log_spec.rb
@@ -5,7 +5,14 @@ RSpec.feature 'Add Log', type: :feature do
   let :cluster { create(:cluster) }
   let :case_details { ['details1', 'details2', 'details3'] }
   let! :cases do
+    create(:case, details: 'Random other case')
     case_details.map { |d| create(:case, cluster: cluster, details: d) }
+  end
+  let! :components do
+    create(:component, name: 'Random other component')
+    ['component1', 'component2', 'component3'].map do |name|
+      create(:component, cluster: cluster, name: name)
+    end
   end
   let :case_select_details do
     cases.map { |c| c.decorate.case_select_details }
@@ -13,37 +20,49 @@ RSpec.feature 'Add Log', type: :feature do
   let :case_select_id { 'log_case_ids' }
   let :case_details_id { 'log_details' }
 
-  before :each do
-    visit cluster_logs_path cluster, as: engineer
-  end
-
   def submit_log
     click_button 'Submit'
     Log.first
   end
 
-  it 'has the case select box' do
-    expect(page).to have_select case_select_id,
-                                options: case_select_details
-  end
-
-  it 'adds a log by the currently logged in engineer' do
-    log_details = 'New log details to make sure the match is correct'
-    fill_in case_details_id, with: log_details
-    log = submit_log
-
-    expect(log).not_to be_nil
-    expect(log.details).to eq(log_details)
-    expect(log.engineer).to eq(engineer)
-    expect(log.cases).to be_blank
-  end
-
-  it 'can add multiple tickets/cases to the view' do
-    fill_in case_details_id, with: 'The details need to be filled'
-    log_cases = [cases.first, cases.last].each do |kase|
-      select kase.decorate.case_select_details, from: case_select_id
+  shared_examples 'shared log features' do
+    it 'has the case select box' do
+      expect(page).to have_select case_select_id,
+                                  options: case_select_details
     end
 
-    expect(submit_log.cases).to contain_exactly(*log_cases)
+    it 'adds a log by the currently logged in engineer' do
+      log_details = 'New log details to make sure the match is correct'
+      fill_in case_details_id, with: log_details
+      log = submit_log
+
+      expect(log).not_to be_nil
+      expect(log.details).to eq(log_details)
+      expect(log.engineer).to eq(engineer)
+      expect(log.cases).to be_blank
+    end
+
+    it 'can add multiple tickets/cases to the view' do
+      fill_in case_details_id, with: 'The details need to be filled'
+      log_cases = [cases.first, cases.last].each do |kase|
+        select kase.decorate.case_select_details, from: case_select_id
+      end
+
+      expect(submit_log.cases).to contain_exactly(*log_cases)
+    end
+  end
+
+  context 'when visiting the cluster log' do
+    before :each do
+      visit cluster_logs_path cluster, as: engineer
+    end
+
+    include_examples 'shared log features'
+
+    it 'has the select component input' do
+      expect(page).to have_select :component_id,
+                                  options: components.map(&:name)
+    end
   end
 end
+
