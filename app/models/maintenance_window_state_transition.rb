fbe4be4b2753a9773be5c97cb71c7546164c4bd2
Bob Whitelock <bob.whitelock1@gmail.com>
2018-02-20 17:59:23 +0000

    Add validations for MW transition user
    User is required to be set for user-initiated transitions, and required
    to not be set for transitions not initiated by a user.

--- app/models/maintenance_window_state_transition.rb
+++ app/models/maintenance_window_state_transition.rb
@@ -3,4 +3,20 @@ class MaintenanceWindowStateTransition < ApplicationRecord
   belongs_to :user, required: false
 
   delegate :site, to: :maintenance_window
+
+  validates_presence_of :user, if: :user_initiated_transition?
+  validates_absence_of :user, unless: :user_initiated_transition?
+
+  private
+
+  USER_INITIATED_STATES = [
+    :cancelled,
+    :confirmed,
+    :rejected,
+    :requested,
+  ].freeze
+
+  def user_initiated_transition?
+    USER_INITIATED_STATES.include?(to.to_sym)
+  end
 end
