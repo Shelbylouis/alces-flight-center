93e4277f6df45759446ecfd537d2764e8f289d71
william <william.mccumstie@outlook.com>
2018-02-20 17:39:54 +0000

    Add a test for the correct number of links
    In the navigation bar there is a bunch of site specific navigation
    links. These links are dynamic and depend which section of flight centre
    the user is visiting.
    
    These links are always within the first 'ul' item. (There are two `ul`
    items, the second contains the logout button). The items themselves are
    `li`. The admins have an additional `all sites` button.

--- spec/features/nav-bar_spec.rb
+++ spec/features/nav-bar_spec.rb
@@ -3,10 +3,17 @@ require 'rails_helper'
 
 RSpec.feature Log, type: :feature do
   let :nav_bar { find_by_id('top-level-navigation-bar') }
+  let :site_nav_bar { nav_bar.all('ul').first }
+  let :site_nav_items { site_nav_bar.all('li') }
 
   shared_examples 'a navigation bar' do
     it 'has the navigation bar' do
       expect(nav_bar.tag_name).to eq('nav')
+    end
+
+    it 'has the correct number of site links' do
+      # Admins have an extract 'all sites' button
+      expect(site_nav_items.length).to eq(subject.length + 1)
     end
   end
 
@@ -17,9 +24,10 @@ RSpec.feature Log, type: :feature do
 
     context 'when visiting the site page' do
       let :scope { create(:site) }
-      before :each { visit_as_admin :site_path }
+      subject { [site_path(scope)] }
 
-      include_examples 'a navigation bar'
+      before :each { visit_as_admin :site_path }
+      it_behaves_like 'a navigation bar'
     end
   end
 end
