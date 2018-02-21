908436a02087659181734850d4550cba6f2d13d1
Bob Whitelock <bob.whitelock1@gmail.com>
2018-02-21 15:55:39 +0000

    Extract MW relation to concern
    Slightly more logical here, as this means anything with this concern
    must have this relation and we do not need to duplicate this relation in
    the individual models.

--- app/models/concerns/cluster_part.rb
+++ app/models/concerns/cluster_part.rb
@@ -9,7 +9,6 @@ module ClusterPart
 
   included do
     has_many :cases
-    has_many :maintenance_windows
 
     validates :name, presence: true
     validates :support_type, inclusion: { in: SUPPORT_TYPES }, presence: true
--- app/models/concerns/has_maintenance_windows.rb
+++ app/models/concerns/has_maintenance_windows.rb
@@ -2,6 +2,10 @@
 module HasMaintenanceWindows
   extend ActiveSupport::Concern
 
+  included do
+    has_many :maintenance_windows
+  end
+
   def open_maintenance_windows
     maintenance_windows.where.not(state: MaintenanceWindow.finished_states)
   end
