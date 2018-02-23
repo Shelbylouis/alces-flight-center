4c0cdd75b5b89c4c0a41374927c5381494eff6e5
william <william.mccumstie@outlook.com>
2018-02-23 18:21:17 +0000

    Generalise the method for finding the scope_id

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -41,6 +41,10 @@ class ApplicationController < ActionController::Base
     raise ActionController::RoutingError.new('Not Found')
   end
 
+  def scope_id_params(id_method)
+    params[id_method] || params[:id]
+  end
+
   def define_navigation_variables
     return unless current_user
 
@@ -50,19 +54,19 @@ class ApplicationController < ActionController::Base
 
     @scope = case request.path
              when /^\/sites/
-               id = params[:site_id] || params[:id]
+               id = scope_id_params(:site_id)
                @site = Site.find(id) if current_user.admin?
              when /^\/clusters/
-               id = params[:cluster_id] || params[:id]
+               id = scope_id_params(:cluster_id)
                @cluster = Cluster.find(id)
              when /^\/components/
-               id = params[:component_id] || params[:id]
+               id = scope_id_params(:component_id)
                @component = @cluster_part = Component.find(id)
              when /^\/component-groups/
-               id = params[:component_group_id] || params[:id]
+               id = scope_id_params(:component_group_id)
                @component_group = ComponentGroup.find(id)
              when /^\/services/
-               id = params[:service_id] || params[:id]
+               id = scope_id_params(:service_id)
                @service = @cluster_part = Service.find(id)
              end
   end
