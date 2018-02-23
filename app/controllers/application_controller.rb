2f12c7e2f83af8a9105228f9d12577c6a1535ef4
william <william.mccumstie@outlook.com>
2018-02-23 16:37:57 +0000

    Assign the scope in the application controller
    The scope is set as the "model" that is currently being visited by the
    page. Please note, not all models support the scope and thus it more
    refers the different parts of the cluster.
    
    Also note, this is different again to `cluster_part` as this does not
    include component_groups for legacy reasons.
    
    We need to get better at naming, sorry

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -44,20 +44,19 @@ class ApplicationController < ActionController::Base
     case request.path
     when /^\/sites/
       id = params[:site_id] || params[:id]
-      @site = Site.find(id) if current_user.admin?
+      @scope = @site = Site.find(id) if current_user.admin?
     when /^\/clusters/
       id = params[:cluster_id] || params[:id]
-      @cluster = Cluster.find(id)
+      @scope = @cluster = Cluster.find(id)
     when /^\/components/
       id = params[:component_id] || params[:id]
-      @component = Component.find(id)
-      @cluster_part = @component
+      @scope = @cluster_part = @component = Component.find(id)
     when /^\/component-groups/
       id = params[:component_group_id] || params[:id]
-      @component_group = ComponentGroup.find(id)
+      @scope = @component_group = ComponentGroup.find(id)
     when /^\/services/
       id = params[:service_id] || params[:id]
-      @cluster_part = Service.find(id)
+      @scope = @cluster_part = Service.find(id)
     end
   end
 
