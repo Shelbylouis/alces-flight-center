2a4dea6aa2c893abe08f14656395d67967338eee
william <william.mccumstie@outlook.com>
2018-02-28 18:55:46 +0000

    Add a list of all cluster components in the navbar
    This is the first step at bringing all the components into the nav-bar.
    The tricky bit for components is that they are grouped.
    
    For consistency, the components nav button displays the entire list. In
    order for this to work, the components table partial needed to be
    updated. It now accepts a list from the controller (otherwise it
    defaults to the previous behaviour).
    
    This is a step towards the controller determining what is displayed
    instead of the view.

--- app/controllers/components_controller.rb
+++ app/controllers/components_controller.rb
@@ -1,6 +1,12 @@
 class ComponentsController < ApplicationController
   decorates_assigned :component
 
+  def index
+    @title = 'Components'
+    @components = @scope.components
+    @table_tile = 'All Components' # Consider removing
+  end
+
   def show
     @title = "#{@component.name} Management Dashboard"
     @subtitle = @component.component_type.name
