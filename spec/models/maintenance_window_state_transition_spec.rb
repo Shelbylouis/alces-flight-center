caa6603f4f7d2b1a62b90d97452ba827dd3fae91
Bob Whitelock <bob.whitelock1@gmail.com>
2018-02-28 17:09:37 +0000

    Indent transition validation tests in block
    So all tests for transition validations will remain grouped together as
    more are added.

--- spec/models/maintenance_window_state_transition_spec.rb
+++ spec/models/maintenance_window_state_transition_spec.rb
@@ -1,27 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe MaintenanceWindowStateTransition, type: :model do
-  describe 'user presence' do
-    user_initiated_states = [:requested, :confirmed, :rejected, :cancelled]
-    automatic_states = [:new, :started, :ended, :expired]
+  describe '#valid?' do
+    describe 'user presence' do
+      user_initiated_states = [:requested, :confirmed, :rejected, :cancelled]
+      automatic_states = [:new, :started, :ended, :expired]
 
-    user_initiated_states.each do |state|
-      context "when transition to `#{state}`" do
-        subject do
-          build(:maintenance_window_state_transition, to: state)
+      user_initiated_states.each do |state|
+        context "when transition to `#{state}`" do
+          subject do
+            build(:maintenance_window_state_transition, to: state)
+          end
+
+          it { is_expected.to validate_presence_of(:user) }
         end
-
-        it { is_expected.to validate_presence_of(:user) }
       end
-    end
 
-    automatic_states.each do |state|
-      context "when transition to `#{state}`" do
-        subject do
-          build(:maintenance_window_state_transition, to: state)
+      automatic_states.each do |state|
+        context "when transition to `#{state}`" do
+          subject do
+            build(:maintenance_window_state_transition, to: state)
+          end
+
+          it { is_expected.to validate_absence_of(:user) }
         end
-
-        it { is_expected.to validate_absence_of(:user) }
       end
     end
   end
