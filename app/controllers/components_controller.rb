e5d2f937ed65fd85efd377e3ef6ddf897dd6aec1
william <william.mccumstie@outlook.com>
2018-03-01 11:37:28 +0000

    Use the type param to select componets to index
    The purpose of the drop down menu is to select which
    `component_groups_types` should be displayed.
    
    This is done with a `type` parameter that is sent with the request. This
    is then converted into the corresponding `groups`. If no type is sent,
    then all the groups are used.
    
    This required reverting the `components_table` partial back to rendering groups.
    
    The maintenance spec needed to be updated as the components are now on a
    different page

--- app/controllers/components_controller.rb
+++ app/controllers/components_controller.rb
@@ -3,12 +3,28 @@ class ComponentsController < ApplicationController
 
   def index
     @title = 'Components'
-    @components = @scope.components
     @table_tile = 'All Components' # Consider removing
+    @component_groups = component_groups_from_type
   end
 
   def show
     @title = "#{@component.name} Management Dashboard"
     @subtitle = @component.component_type.name
+  end
+
+  private
+
+  def component_groups_from_type
+    if type_param.blank?
+      @scope.component_groups
+    else
+      @scope.component_groups_by_type.find do |group_type|
+        group_type.name == type_param[:type]
+      end.component_groups
+    end
+  end
+
+  def type_param
+    params.permit(:type)
   end
 end
