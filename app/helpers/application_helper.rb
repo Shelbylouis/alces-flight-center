8e9affa79626502f444a28d1988df833b7552119
william <william.mccumstie@outlook.com>
2018-03-02 11:10:14 +0000

    Generalise how the tab partial is rendered
    Instead of having two separate renders within the `if`, the `if` selects
    the template and then passes it to a single render

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -47,11 +47,12 @@ module ApplicationHelper
   end
 
   def render_tab_bar(**render_args, &b)
-    if @scope.is_a? Cluster
-      render('clusters/tabs', **render_args, &b)
-    else
-      render('partials/card', **render_args, &b)
-    end
+    template = if @scope.is_a? Cluster
+                 'clusters/tabs'
+               else
+                 'partials/card'
+               end
+    render(template, **render_args, &b)
   end
 
   def dark_button_classes
