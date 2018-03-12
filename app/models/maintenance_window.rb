80969d46e832bb64f0e6d95def69d7e4498abe8a
Bob Whitelock <bob.whitelock1@gmail.com>
2018-03-12 12:54:24 +0000

    Extract MaintenanceWindow Validator class
    So main MaintenanceWindow class clearer and can confine all methods only
    needed for validation to one location.

--- app/models/maintenance_window.rb
+++ app/models/maintenance_window.rb
@@ -7,10 +7,9 @@ class MaintenanceWindow < ApplicationRecord
   has_many :maintenance_window_state_transitions
   alias_attribute :transitions, :maintenance_window_state_transitions
 
-  validate :validate_precisely_one_associated_model
   validates_presence_of :requested_start
   validates_presence_of :requested_end
-  validate :validate_requested_period
+  validates_with Validator
 
   scope :unfinished, -> { where.not(state: finished_states) }
 
@@ -108,54 +107,6 @@ class MaintenanceWindow < ApplicationRecord
 
   def maintenance_notifier
     @maintenance_notifier ||= MaintenanceNotifier.new(self)
-  end
-
-  def validate_precisely_one_associated_model
-    errors.add(
-      :base, 'precisely one Cluster, Component, or Service can be under maintenance'
-    ) unless number_associated_models == 1
-  end
-
-  def number_associated_models
-    [cluster, component, service].select(&:present?).length
-  end
-
-  def validate_requested_period
-    return unless requested_start && requested_end
-    validate_start_before_end
-    validate_start_or_end_in_future_if_needed
-  end
-
-  def validate_start_before_end
-    if requested_start > requested_end
-      errors.add(:requested_end, 'must be after start')
-    end
-  end
-
-  def validate_start_or_end_in_future_if_needed
-    return if maintenance_period_can_be_passed?
-    validate_field_in_future(:requested_start)
-    validate_field_in_future(:requested_end)
-  end
-
-  def maintenance_period_can_be_passed?
-    # In any of these states the maintenance period can be passed, otherwise
-    # existing saved MaintenanceWindows would become invalidated.
-    #
-    # Note in particular that the maintenance period can be passed when
-    # maintenance is only `started` to allow maintenance which has not been
-    # progressed on time to be caught up to the state it should be in (e.g. if
-    # we did not progress MWs for a while for some reason); without this the MW
-    # would be invalid when the `confirmed` -> `started` transition happened,
-    # as it should already be `ended`.
-    [cancelled?, ended?, expired?, rejected?, started?].any?
-  end
-
-  def validate_field_in_future(field_name)
-    field = send(field_name)
-    if field.past?
-      errors.add(field_name, 'cannot be in the past')
-    end
   end
 
   # Represents a query for the value of a particular property of a
