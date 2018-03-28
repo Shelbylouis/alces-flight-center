1aef0e0bf37ed0a99465ee6c63ecb0ca6a1c85b4
william <william.mccumstie@outlook.com>
2018-03-28 15:10:52 +0100

    Replace case_form_path with the scope_path methods
    This allows it to be dynamically created in all circumstances

--- app/decorators/application_decorator.rb
+++ app/decorators/application_decorator.rb
@@ -32,7 +32,7 @@ class ApplicationDecorator < Draper::Decorator
   # entire Cluster, but I don't have a better name for that yet.
   def cluster_part_case_form_buttons
     buttons = [
-      case_form_button(case_form_path, disabled: advice?),
+      case_form_button(new_scope_case_path, disabled: advice?),
       consultancy_form_button(consultancy_form_path)
     ].join
     h.raw(buttons)
@@ -127,11 +127,6 @@ class ApplicationDecorator < Draper::Decorator
   def new_maintenance_window_path
     link_helper = "new_#{readable_model_name}_maintenance_window_path"
     h.send(link_helper, self)
-  end
-
-  def case_form_path
-    helper = "new_#{readable_model_name}_case_path"
-    h.send(helper, id_key => self.id)
   end
 
   def consultancy_form_path
