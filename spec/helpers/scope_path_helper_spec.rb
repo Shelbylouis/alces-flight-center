b6dfae65688250e3f4dfc537db224d69219b157c
william <william.mccumstie@outlook.com>
2018-03-28 13:12:28 +0100

    Update the spec to be in different contexts
    The behaviour of the scope_path module needs to distinguish between
    admins and contacts when it comes to the site paths.
    
    This has not been implemented yet, however this commit organises the
    spec into contexts so it can be tested in the future.

--- spec/helpers/scope_path_helper_spec.rb
+++ spec/helpers/scope_path_helper_spec.rb
@@ -1,22 +1,48 @@
 require 'rails_helper'
 
 RSpec.describe ScopePathHelper do
-  describe '#respond_to' do
-    it 'responds to #scope_path' do
-      expect(helper.respond_to? :scope_path).to eq(true)
+  before :each do
+    allow(helper).to receive(:current_user).and_return(user)
+  end
+
+  context 'with a contact logged in' do
+    let :user { create(:contact) }
+
+    describe '#respond_to' do
+      it 'responds to #scope_path' do
+        expect(helper.respond_to? :scope_path).to eq(true)
+      end
+
+      it 'does not respond to #_scope_path' do
+        expect(helper.respond_to? :_scope_path).to eq(false)
+      end
     end
 
-    it 'does not respond to #_scope_path' do
-      expect(helper.respond_to? :_scope_path).to eq(false)
+    describe '#scope_path' do
+      subject { helper.scope_path(scope) }
+
+      context 'when in a cluster scope' do
+        let :scope { create(:cluster) }
+
+        it 'finds the path' do
+          expect(subject).to eq(helper.cluster_path(scope))
+        end
+      end
     end
   end
 
-  context 'when in a cluster scope' do
-    let :scope { create(:cluster) }
+  context 'with an admin logged in' do
+    let :user { create(:admin) }
 
     describe '#scope_path' do
-      it 'finds the path' do
-        expect(helper.scope_path(scope)).to eq(helper.cluster_path(scope))
+      subject { helper.scope_path(scope) }
+
+      context 'when in a site scope' do
+        let :scope { create(:site) }
+
+        it 'returns the site path' do
+          expect(subject).to eq(site_path(scope))
+        end
       end
     end
   end
