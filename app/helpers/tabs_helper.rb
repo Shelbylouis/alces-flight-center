52dca89c50e79c8d1d3001c852252490f29cfc20
william <william.mccumstie@outlook.com>
2018-03-28 14:47:36 +0100

    Remove the `scope_path` method from TabsHelper
    It has now been replaced by the decorator methods

--- app/helpers/tabs_helper.rb
+++ app/helpers/tabs_helper.rb
@@ -1,51 +1,31 @@
 module TabsHelper
   class TabsBuilder
     def initialize(scope, h)
-      @scope = scope
+      @scope = scope.decorate
       @h = h
     end
 
     def overview
-      { id: :overview, path: scope_path }
+      { id: :overview, path: scope.scope_path }
     end
 
     def cases
       {
-        id: :cases, path: scope_path('cases'),
+        id: :cases, path: scope.scope_cases_path,
         dropdown: [
-          { text: 'Current', path: scope_path('cases') },
-          { text: 'Archive', path: scope_path('cases', archive: true) }
+          { text: 'Current', path: scope.scope_cases_path },
+          { text: 'Archive', path: scope.scope_cases_path(archive: true) }
         ]
       }
     end
 
     def asset_record
-      { id: :asset_record, path: scope_path('asset_record') }
+      { id: :asset_record, path: scope.scope_asset_record_path }
     end
 
     private
 
     attr_reader :scope, :h
-
-    def page_is_a_contacts_site?
-      scope.is_a?(Site) && h.current_user.contact?
-    end
-
-    # Regular contacts have their site as the root path and thus need to be
-    # handled separately
-    def scope_path(method = nil, **hash_params)
-      method_args = begin
-        if page_is_a_contacts_site?
-          [(method ? method : 'root') + '_path']
-        else
-          method_string = [
-            scope.class.name.underscore, method, 'path'
-          ].reject(&:nil?).join('_')
-          [method_string, scope]
-        end
-      end
-      h.send(*method_args, **hash_params)
-    end
   end
 end
 
