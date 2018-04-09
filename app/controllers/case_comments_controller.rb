50a72e5f111ed3ccbf6e02937fc6e409f2ec6275
James Muscat <james.muscat@alces-software.com>
2018-04-09 16:55:36 +0100

    Move permission logic into model; return sane-ish errors to user.

--- app/controllers/case_comments_controller.rb
+++ app/controllers/case_comments_controller.rb
@@ -5,15 +5,11 @@ class CaseCommentsController < ApplicationController
     case_id = params[:case_id]
     my_case = Case.find(case_id)
 
-    if my_case.site == current_user.site || current_user.admin?
-      new_comment = my_case.case_comments.create(user: current_user, text: params[:case_comment][:text])
-      if new_comment.persisted?
-        flash[:notice] = 'New comment added.'
-      else
-        flash[:error] = 'Your comment was not added.'
-      end
+    new_comment = my_case.case_comments.create(user: current_user, text: params[:case_comment][:text])
+    if new_comment.persisted?
+      flash[:notice] = 'New comment added.'
     else
-      flash[:error] = 'You do not have permission to comment on this case.'
+      flash[:error] = "Your comment was not added. #{new_comment.errors.full_messages.join('; ').strip}"
     end
 
     redirect_to case_path(case_id)
--- app/models/case_comment.rb
+++ app/models/case_comment.rb
@@ -8,4 +8,12 @@ class CaseComment < ApplicationRecord
   validates :case, presence: true
 
   validates :text, length: { minimum: 2}
+
+  validate :valid_user, on: :create
+
+  private
+
+  def valid_user
+    errors.add(:user, 'does not have permission to post a comment on this case') unless user.admin? || user.site == site
+  end
 end
