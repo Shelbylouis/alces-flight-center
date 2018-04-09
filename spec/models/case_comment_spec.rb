2ee95f54dc9e58bd8078d62bd147e5735204863e
James Muscat <james.muscat@alces-software.com>
2018-04-09 17:49:42 +0100

    Add some tests for `CaseComment`s.

--- spec/models/case_comment_spec.rb
+++ spec/models/case_comment_spec.rb
@@ -1,5 +1,58 @@
 require 'rails_helper'
 
 RSpec.describe CaseComment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before :each do
+    @site = create(:site)
+    cluster = create(:cluster, site: @site)
+    @case = create(:case, cluster: cluster)
+  end
+
+  describe '#create' do
+    it 'prevents empty comments' do
+      admin = create(:admin)
+      expect do
+        CaseComment.create!(
+           case: @case,
+           user: admin,
+           text: ''
+        )
+      end.to raise_error(ActiveRecord::RecordInvalid)
+    end
+
+    it 'prevents users from other sites commenting' do
+      other_site = create(:site)
+      other_user = create(:user, site: other_site)
+
+      expect do
+        CaseComment.create!(
+           case: @case,
+           user: other_user,
+           text: 'This comment is not allowed'
+        )
+      end.to raise_error(ActiveRecord::RecordInvalid)
+    end
+
+    it 'allows users from the correct site to comment' do
+      my_user = create(:user, site: @site)
+
+      my_comment = CaseComment.create!(
+         case: @case,
+         user: my_user,
+         text: 'This comment is allowed'
+      )
+
+      expect(@case.case_comments.first).to eq(my_comment)
+    end
+
+    it 'allows admins to comment' do
+      my_comment = CaseComment.create!(
+         case: @case,
+         user: create(:admin),
+         text: 'This comment is allowed'
+      )
+
+      expect(@case.case_comments.first).to eq(my_comment)
+    end
+  end
 end
