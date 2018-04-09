468b1381a44a87294d9d509ca4185fb5f7e19cb3
James Muscat <james.muscat@alces-software.com>
2018-04-09 16:30:24 +0100

    Form for adding comments to cases.

--- app/controllers/case_comments_controller.rb
+++ app/controllers/case_comments_controller.rb
@@ -2,12 +2,16 @@ class CaseCommentsController < ApplicationController
   before_action :require_login
 
   def create
-    case_id = params[:id]
+    case_id = params[:case_id]
     my_case = Case.find(case_id)
 
     if my_case.site == current_user.site || current_user.admin?
-      my_case.case_comments.create(user: current_user, text: params[:text])
-      flash[:notice] = 'New comment added.'
+      new_comment = my_case.case_comments.create(user: current_user, text: params[:case_comment][:text])
+      if new_comment.persisted?
+        flash[:notice] = 'New comment added.'
+      else
+        flash[:error] = 'Your comment was not added.'
+      end
     else
       flash[:error] = 'You do not have permission to comment on this case.'
     end
