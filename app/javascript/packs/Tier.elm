6efb8f62210fc03e329f5b379c96542820296a1d
Bob Whitelock <bob.whitelock1@gmail.com>
2018-04-11 13:30:01 +0100

    Properly handle decoding of Tier data

--- app/javascript/packs/Tier.elm
+++ app/javascript/packs/Tier.elm
@@ -1,4 +1,4 @@
-module Tier exposing (..)
+module Tier exposing (Tier, decoder, levelAsInt)
 
 import Json.Decode as D
 
@@ -47,12 +47,75 @@ type
 
 decoder : D.Decoder Tier
 decoder =
-    -- XXX Actually decode Tiers
-    D.succeed
-        { id = Id -1
-        , level = One
-        , fields = []
-        }
+    let
+        intermediateData =
+            \id levelResult fields ->
+                { id = id
+                , levelResult = levelResult
+                , fields = fields
+                }
+
+        createTier =
+            \intermediate ->
+                case intermediate.levelResult of
+                    Ok level ->
+                        D.succeed <|
+                            Tier intermediate.id level intermediate.fields
+
+                    Err error ->
+                        D.fail error
+    in
+    D.map3 intermediateData
+        (D.field "id" D.int |> D.map Id)
+        (D.field "level" D.int |> D.map intToLevel)
+        (D.field "fields" <| D.list fieldDecoder)
+        |> D.andThen createTier
+
+
+fieldDecoder : D.Decoder Field
+fieldDecoder =
+    let
+        fieldTypeDecoder =
+            \type_ ->
+                case type_ of
+                    "markdown" ->
+                        markdownDecoder
+
+                    "input" ->
+                        textInputDecoder Input
+
+                    "textarea" ->
+                        textInputDecoder Textarea
+
+                    _ ->
+                        D.fail <| "Invalid type: " ++ type_
+    in
+    D.field "type" D.string
+        |> D.andThen fieldTypeDecoder
+
+
+markdownDecoder : D.Decoder Field
+markdownDecoder =
+    D.map Markdown <| D.field "content" D.string
+
+
+textInputDecoder : TextInputType -> D.Decoder Field
+textInputDecoder type_ =
+    let
+        initialValueDecoder =
+            D.succeed ""
+
+        optionalDecoder =
+            D.field "optional" D.bool
+                |> D.maybe
+                |> D.map (Maybe.withDefault False)
+    in
+    D.map TextInput <|
+        D.map4 TextInputData
+            (D.succeed type_)
+            (D.field "name" D.string)
+            initialValueDecoder
+            optionalDecoder
 
 
 levelAsInt : Tier -> Int
@@ -69,3 +132,22 @@ levelAsInt tier =
 
         Three ->
             3
+
+
+intToLevel : Int -> Result String Level
+intToLevel int =
+    case int of
+        0 ->
+            Ok Zero
+
+        1 ->
+            Ok One
+
+        2 ->
+            Ok Two
+
+        3 ->
+            Ok Three
+
+        _ ->
+            Err <| "Invalid level: " ++ toString int
