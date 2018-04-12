3263408ea781a07c9b033b75dc4e5539f7d28f9e
James Muscat <james.muscat@alces-software.com>
2018-04-12 17:10:57 +0100

    Pull email styles from `flight-sso`.

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,11 @@
+require_dependency 'alces/mailer/resender'
+require_dependency 'alces/action_view/templates'
+
 class ApplicationMailer < ActionMailer::Base
+  include Roadie::Rails::Automatic
+  extend Alces::Mailer::Resender
+
   default from: 'support@alces-flight.com'
   layout 'mailer'
+  helper 'mailer'
 end
