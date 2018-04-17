df25456ed4c185dd5699c028a4351c62fd088009
Bob Whitelock <bob.whitelock1@gmail.com>
2018-04-17 16:37:18 +0100

    Extract shared_examples for similar feature tests

--- spec/features/services_spec.rb
+++ spec/features/services_spec.rb
@@ -1,46 +1,5 @@
 require 'rails_helper'
 
 RSpec.feature '/services/', type: :feature do
-  before :each do
-    # Prevent attempting to retrieve documents from S3 when Cluster page
-    # visited.
-    allow_any_instance_of(Cluster).to receive(:documents).and_return([])
-
-    create(:request_service_becomes_managed_issue)
-    create(:request_service_becomes_advice_issue)
-  end
-
-  let :site { create(:site) }
-  let :contact { create(:contact, site: site) }
-  let :cluster { create(:cluster, site: site) }
-
-  it 'can request self-management of a managed Service' do
-    service = create(:managed_service, cluster: cluster)
-
-    visit cluster_services_path(cluster, as: contact)
-    click_button 'Request self-management'
-
-    created_case = service.cases.first
-    expect(created_case.service).to eq(service)
-    issue = Issue.request_service_becomes_advice_issue
-    expect(created_case.issue).to eq(issue)
-    tier = issue.tiers.first
-    expect(created_case.fields).to eq(tier.fields)
-    expect(created_case.tier_level).to eq(tier.level)
-  end
-
-  it 'can request Alces management of an advice-only Service' do
-    service = create(:advice_service, cluster: cluster)
-
-    visit cluster_services_path(cluster, as: contact)
-    click_button 'Request Alces management'
-
-    created_case = service.cases.first
-    expect(created_case.service).to eq(service)
-    issue = Issue.request_service_becomes_managed_issue
-    expect(created_case.issue).to eq(issue)
-    tier = issue.tiers.first
-    expect(created_case.fields).to eq(tier.fields)
-    expect(created_case.tier_level).to eq(tier.level)
-  end
+  it_behaves_like 'can request support_type change via buttons', :service
 end
