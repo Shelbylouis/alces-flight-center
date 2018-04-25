181979c2a75e5ccb7fa45481f8bf8115bca24868
Bob Whitelock <bob.whitelock1@gmail.com>
2018-04-25 09:14:57 +0100

    Break tier_level validation specs into contexts
    So can easily slot in new tests with same setup.

--- spec/models/case/available_support_validator_spec.rb
+++ spec/models/case/available_support_validator_spec.rb
@@ -5,31 +5,36 @@ RSpec.describe Case, type: :model do
     [:component, :service, :cluster].each do |part_name|
       context "when associated with `advice` #{part_name}" do
         subject do
-          build("case_requiring_#{part_name}", part_name => part)
+          build(
+            "case_requiring_#{part_name}",
+            part_name => part,
+            tier_level: tier_level
+          )
         end
 
         let :part { create("advice_#{part_name}") }
 
-        it 'is invalid when tier_level < 3' do
-          subject.tier_level = 2
+        context 'when tier_level == 3' do
+          let :tier_level { 3 }
 
-          expect(subject).to be_invalid
-          expect(subject.errors.messages).to include(
-            part_name => [/is self-managed, only consultancy support may be requested/]
-          )
+          it { is_expected.to be_valid }
         end
 
-        it 'is valid when tier_level == 3' do
-          subject.tier_level == 3
+        context 'when tier_level < 3' do
+          let :tier_level { 2 }
 
-          expect(subject).to be_valid
-        end
+          it 'should normally be invalid' do
+            expect(subject).to be_invalid
+            expect(subject.errors.messages).to include(
+              part_name => [/is self-managed, only consultancy support may be requested/]
+            )
+          end
 
-        it 'is valid when tier_level < 3 and Issue is special' do
-          subject.tier_level = 2
-          subject.issue.identifier = Issue::IDENTIFIER_NAMES.first
+          it 'should be valid when Issue is special' do
+            subject.issue.identifier = Issue::IDENTIFIER_NAMES.first
 
-          expect(subject).to be_valid
+            expect(subject).to be_valid
+          end
         end
       end
     end
