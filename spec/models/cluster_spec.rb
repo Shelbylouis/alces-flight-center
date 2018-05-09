392fd6d4e3b462320f247adc06ce081c6c057fac
Bob Whitelock <bob.whitelock1@gmail.com>
2018-05-09 13:20:15 +0100

    Temporarily disable migration so import succeeds

--- spec/models/cluster_spec.rb
+++ spec/models/cluster_spec.rb
@@ -7,7 +7,11 @@ RSpec.describe Cluster, type: :model do
   describe '#valid?' do
     subject { create(:cluster) }
 
-    it { is_expected.to validate_presence_of(:motd) }
+    # XXX Add this back once current staging deployed and so `rake
+    # alces:data:import_and_migrate_production` should succeed with this set
+    # (failing right now due to multiple Cluster data migrations since last
+    # deploy).
+    # it { is_expected.to validate_presence_of(:motd) }
 
     context 'when managed cluster' do
       subject do
