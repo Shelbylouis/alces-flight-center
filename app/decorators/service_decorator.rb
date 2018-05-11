66b48d8fb49967ce17810e38dd0afd44e7d87ac5
Bob Whitelock <bob.whitelock1@gmail.com>
2018-05-11 13:09:07 +0100

    Extract class to build issues JSON for Service
    All these methods were only used when building the issues JSON, so might
    as well confine them within a class to make this clearer and indicate
    they may not be generally useful.

--- app/decorators/service_decorator.rb
+++ app/decorators/service_decorator.rb
@@ -15,42 +15,58 @@ class ServiceDecorator < ClusterPartDecorator
   end
 
   def case_form_json
-    super.merge(categories_or_issues)
+    issues_json = IssuesJsonBuilder.build_for(self)
+    super.merge(issues_json)
   end
 
   private
 
-  def categories_or_issues
-    if any_categorised_issues?
-      {categories: categorised_applicable_issues}
-    else
-      {issues: json_for_issues(applicable_issues)}
+  class IssuesJsonBuilder
+    def self.build_for(service)
+      new(service).build
     end
-  end
 
-  def any_categorised_issues?
-    applicable_issues.any?(&:category)
-  end
+    def build
+      if any_categorised_issues?
+        {categories: categorised_applicable_issues}
+      else
+        {issues: json_for_issues(applicable_issues)}
+      end
+    end
 
-  def categorised_applicable_issues
-    applicable_issues
-      .group_by(&:category)
-      .transform_keys do |category|
-      category.nil? ? Category.new(name: 'Other', id: -1) : category
-    end.map do |category, issues|
-      category.decorate.case_form_json.merge(issues: json_for_issues(issues))
-    end.reject(&:nil?)
-  end
+    private
 
-  def applicable_issues
-    issues_requiring_any_service = Issue.where(
-      requires_service: true,
-      service_type: nil
-    )
-    (service_type.issues + issues_requiring_any_service).reject(&:special?)
-  end
+    attr_reader :service
+    delegate :service_type, to: :service
 
-  def json_for_issues(issues)
-    issues.map { |issue| issue.decorate.case_form_json }
+    def initialize(service)
+      @service = service
+    end
+
+    def any_categorised_issues?
+      applicable_issues.any?(&:category)
+    end
+
+    def categorised_applicable_issues
+      applicable_issues
+        .group_by(&:category)
+        .transform_keys do |category|
+        category.nil? ? Category.new(name: 'Other', id: -1) : category
+      end.map do |category, issues|
+        category.decorate.case_form_json.merge(issues: json_for_issues(issues))
+      end.reject(&:nil?)
+    end
+
+    def applicable_issues
+      issues_requiring_any_service = Issue.where(
+        requires_service: true,
+        service_type: nil
+      )
+      (service_type.issues + issues_requiring_any_service).reject(&:special?)
+    end
+
+    def json_for_issues(issues)
+      issues.map { |issue| issue.decorate.case_form_json }
+    end
   end
 end
