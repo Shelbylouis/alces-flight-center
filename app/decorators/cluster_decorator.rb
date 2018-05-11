33f337e2fef36b9bc84d5f4b0d23fe47806af89f
Bob Whitelock <bob.whitelock1@gmail.com>
2018-05-11 12:09:48 +0100

    Extract Cluster#case_form_json to decorator
    This is more of a view concern, it just was first added before we
    started using Draper.

--- app/decorators/cluster_decorator.rb
+++ app/decorators/cluster_decorator.rb
@@ -29,4 +29,21 @@ class ClusterDecorator < ApplicationDecorator
       }
     ]
   end
+
+  def case_form_json
+    {
+      id: id,
+      name: name,
+      components: components.map(&:case_form_json),
+      services: services.map(&:case_form_json),
+      supportType: support_type,
+      chargingInfo: charging_info,
+      # Encode MOTD in two forms: the raw form, to be used as the initial value
+      # to be edited in the MOTD tool, and as sanitized, formatted HTML so the
+      # current value can be displayed as it will be on the Cluster and in the
+      # rest of Flight Center.
+      motd: motd,
+      motdHtml: simple_format(motd),
+    }
+  end
 end
--- spec/models/cluster_spec.rb
+++ spec/models/cluster_spec.rb
@@ -64,42 +64,6 @@ RSpec.describe Cluster, type: :model do
     end
   end
 
-  describe '#case_form_json' do
-    subject do
-      create(
-        :cluster,
-        id: 1,
-        name: 'Some Cluster',
-        support_type: :managed,
-        charging_info: '£1000',
-        motd: 'Some MOTD',
-      ).tap do |cluster|
-        cluster.components = [create(:component, cluster: cluster)]
-        cluster.services = [create(:service, cluster: cluster)]
-        cluster.cases = [
-          create(:case)
-        ]
-      end
-    end
-
-    it 'gives correct JSON' do
-      text_helper = Class.new do
-        include ActionView::Helpers::TextHelper
-      end.new
-
-      expect(subject.case_form_json).to eq(
-        id: 1,
-        name: 'Some Cluster',
-        components: subject.components.map(&:case_form_json),
-        services: subject.services.map(&:case_form_json),
-        supportType: 'managed',
-        chargingInfo: '£1000',
-        motd: 'Some MOTD',
-        motdHtml: text_helper.simple_format('Some MOTD')
-      )
-    end
-  end
-
   describe '#managed_components' do
     subject do
       create(:cluster, support_type: 'managed') do |cluster|
