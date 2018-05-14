8c3a98c8826e618863780154e28fe55599878ebc
Andrew <andrew.toghill@alces-software.com>
2018-05-14 15:31:07 +0100

    Add cases dropdown to the nav tab

--- app/decorators/all_sites_decorator.rb
+++ app/decorators/all_sites_decorator.rb
@@ -1,3 +1,16 @@
 class AllSitesDecorator < ApplicationDecorator
   delegate_all
+
+  def tabs
+    [
+      #{ id: :overview, path: h.cases_path },
+      {
+        id: :cases, path: h.cases_path,
+        dropdown: [
+          { text: 'Current', path: h.cases_path },
+          { text: 'Resolved', path: h.resolved_cases_path }
+        ]
+      }
+    ]
+  end
 end
