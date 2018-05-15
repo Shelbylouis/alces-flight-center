7276d658cdf4827db8f2c186c2da871698f2a949
Bob Whitelock <bob.whitelock1@gmail.com>
2018-05-15 11:59:59 +0100

    Refactor and simplify generating nav links
    Rather than one long method with various conditionals, can instead split
    this up into various methods which either give a 'nav link Proc' or nil
    depending on whether the link should be displayed, and then just compact
    these together to get the overall array to generate the nav bar.

--- app/helpers/scope_nav_links_builder.rb
+++ app/helpers/scope_nav_links_builder.rb
@@ -7,51 +7,60 @@ class ScopeNavLinksBuilder
   end
 
   def build
-    return [] unless scope
-    scope_nav_link_procs = []
-
-    if h.current_user&.admin?
-      scope_nav_link_procs << nav_link_proc(text: 'All Sites',
-                                            path: h.root_path,
-                                            nav_icon: 'fa-globe')
-    end
-
-    site_obj = model_from_scope :site
-    path_for_site = if h.current_user.admin?
-                      site_obj
-                    else
-                      h.root_path
-                    end
-    if site_obj
-      scope_nav_link_procs << nav_link_proc(model: site_obj,
-                                            path: path_for_site,
-                                            nav_icon: 'fa-institution')
-    end
-
-    cluster_obj = model_from_scope :cluster
-    if cluster_obj
-      scope_nav_link_procs << nav_link_proc(model: cluster_obj,
-                                            nav_icon: 'fa-server')
-    end
-
-    component_group_obj = model_from_scope :component_group
-    if component_group_obj
-      scope_nav_link_procs << nav_link_proc(model: component_group_obj,
-                                            nav_icon: 'fa-cubes')
-    end
-
-    cluster_part = model_from_scope(:service) || model_from_scope(:component)
-    if cluster_part
-      scope_nav_link_procs << nav_link_proc(model: cluster_part,
-                                            nav_icon: 'fa-cube')
-    end
-
-    scope_nav_link_procs
+    [
+      all_sites_link,
+      site_link,
+      cluster_link,
+      component_group_link,
+      cluster_part_link,
+    ].compact
   end
 
   private
 
   attr_reader :scope
+
+  def all_sites_link
+    if h.current_user&.admin?
+      nav_link_proc(text: 'All Sites',
+                    path: h.root_path,
+                    nav_icon: 'fa-globe')
+    end
+  end
+
+  def site_link
+    site_obj = model_from_scope :site
+    return nil unless site_obj
+    path_for_site = if h.current_user.admin?
+                      site_obj
+                    else
+                      h.root_path
+                    end
+    nav_link_proc(model: site_obj,
+                  path: path_for_site,
+                  nav_icon: 'fa-institution')
+  end
+
+  def cluster_link
+    cluster_obj = model_from_scope :cluster
+    return nil unless cluster_obj
+    nav_link_proc(model: cluster_obj,
+                  nav_icon: 'fa-server')
+  end
+
+  def component_group_link
+    component_group_obj = model_from_scope :component_group
+    return nil unless component_group_obj
+    nav_link_proc(model: component_group_obj,
+                  nav_icon: 'fa-cubes')
+  end
+
+  def cluster_part_link
+    cluster_part = model_from_scope(:service) || model_from_scope(:component)
+    return nil unless cluster_part
+    nav_link_proc(model: cluster_part,
+                  nav_icon: 'fa-cube')
+  end
 
   def model_from_scope(type)
     if scope.respond_to? type
