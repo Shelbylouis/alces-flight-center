29c25596787ddb2f22927d824e3bd72a4098dbaf
Andrew <andrew.toghill@alces-software.com>
2018-05-16 16:24:29 +0100

    Use a modified version of the cases tab
    This decorator now utilises the cases tab within the tab builder but
    with some distinct differences. There is a method to strip away the
    create option in the dropdown which isn't needed for this part of the
    site. It also changes the text of the dropdown to indicate it is for all
    cases.

--- app/decorators/all_sites_decorator.rb
+++ app/decorators/all_sites_decorator.rb
@@ -4,13 +4,26 @@ class AllSitesDecorator < ApplicationDecorator
   def tabs
     [
       { id: :all_sites, path: h.root_path },
-      {
-        id: :cases, text: 'All Cases', path: h.cases_path,
-        dropdown: [
-          { text: 'Current', path: h.cases_path },
-          { text: 'Resolved', path: h.resolved_cases_path }
-        ]
-      }
+      cases_tab
     ]
+  end
+
+  private
+
+  def cases_tab
+    tabs_builder.cases.merge(text: 'All Cases').tap do |tab|
+      tab[:dropdown] = tab[:dropdown].reject do |item|
+        item[:text] == 'Create'
+      end
+    end
+  end
+
+  # We override these to generate the top level path names
+  def scope_name_for_paths
+    ''
+  end
+
+  def arguments_for_scope_path(*a)
+    a
   end
 end
