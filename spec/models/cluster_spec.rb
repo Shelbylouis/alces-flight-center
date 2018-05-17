9b629492eab76498337e00ec01044226052479a2
Andrew <andrew.toghill@alces-software.com>
2018-05-17 15:08:20 +0100

    Removed redundant tests
    As the methods being described no longer exist within the cluster model
    the associated tests have been removed. However the functionality being
    tested has been moved to a shared example in the previous commit.

--- spec/models/cluster_spec.rb
+++ spec/models/cluster_spec.rb
@@ -64,39 +64,6 @@ RSpec.describe Cluster, type: :model do
     end
   end
 
-  describe '#managed_components' do
-    subject do
-      create(:cluster, support_type: 'managed') do |cluster|
-        3.times { create(:component, cluster: cluster, support_type: 'inherit') }
-        create(:component, cluster: cluster, support_type: 'managed')
-        create(:component, cluster: cluster, support_type: 'advice')
-      end
-    end
-
-    it 'returns all managed cluster components for cluster' do
-      result = subject.components.managed
-      expect(result).to all be_a(Component)
-      expect(result.length).to be 4
-    end
-  end
-
-  describe '#advice_components' do
-    subject do
-      create(:cluster, support_type: 'advice') do |cluster|
-        4.times { create(:component, cluster: cluster, support_type: 'inherit') }
-        create(:component, cluster: cluster, support_type: 'managed')
-        create(:component, cluster: cluster, support_type: 'advice')
-        cluster.reload
-      end
-    end
-
-    it 'returns all advice cluster components for cluster' do
-      result = subject.components.advice
-      expect(result).to all be_a(Component)
-      expect(result.length).to eq 5
-    end
-  end
-
   context 'cluster document tests' do
     subject { create(:cluster, name: 'Some Cluster', site: site) }
     let :site { create(:site, name: 'The Site') }
