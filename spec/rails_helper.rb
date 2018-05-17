e77205ffddddc99524d81ca39d2be1fa4098c9b2
Bob Whitelock <bob.whitelock1@gmail.com>
2018-05-17 12:36:53 +0100

    Increase Capybara Selenium driver timeout
    Override Capybara Selenium driver with increased timeout, to (hopefully)
    avoid intermittent failures on tiny CI server.
    
    See
    https://github.com/alces-software/alces-flight-center/pull/272#issuecomment-389589269,
    https://alces.slack.com/archives/C72GT476Y/p1526556655000298.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -58,6 +58,16 @@ Capybara.add_selector(:test_element) do
   end
 end
 
+# Override Capybara Selenium driver with increased timeout, to (hopefully)
+# avoid intermittent failures on tiny CI server (see
+# https://stackoverflow.com/a/33898828/2620402).
+Capybara.register_driver :selenium do |app|
+  profile = Selenium::WebDriver::Firefox::Profile.new
+  client = Selenium::WebDriver::Remote::Http::Default.new
+  client.timeout = 120 # Instead of default 60s timeout.
+  Capybara::Selenium::Driver.new(app, browser: :firefox, profile: profile, http_client: client)
+end
+
 # Convenience function to use above.
 def test_element(data_test_value)
   find(:test_element, data_test_value)
