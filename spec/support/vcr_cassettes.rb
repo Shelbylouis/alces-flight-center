872f8142d34b9c44319ca815a80be66a62622744
James Muscat <james.muscat@alces-software.com>
2018-05-21 11:07:37 +0100

    Remove legacy RT-integration code.

--- spec/support/vcr_cassettes.rb
+++ spec/support/vcr_cassettes.rb
@@ -1,8 +1,4 @@
 
 module VcrCassettes
   S3_READ_DOCUMENTS='s3_read_documents'
-
-  RT_CREATE_TICKET='rt_create_ticket'
-  RT_ADD_TICKET_CORRESPONDENCE='rt_add_ticket_correspondence'
-  RT_SHOW_TICKET='rt_show_ticket'
 end
