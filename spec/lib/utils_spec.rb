fa303e52d18552785f291ca2c1e4336900720b3d
Bob Whitelock <bob.whitelock1@gmail.com>
2018-05-24 13:14:17 +0100

    Remove now unused `rt_format` method
    We have now removed RT integration, and no longer use this method.

--- spec/lib/utils_spec.rb
+++ spec/lib/utils_spec.rb
@@ -3,28 +3,6 @@ require 'rails_helper'
 require 'utils'
 
 RSpec.describe Utils do
-  describe '#rt_format' do
-    it 'appropriately formats a hash as text' do
-      expect(
-        described_class.rt_format(foo: 'bar',
-                                  multiline_string: <<-EOF.strip_heredoc,
-          is
-          appropriately
-          indented
-          EOF
-                                  another_key: 'value')
-      ).to eq(
-        <<-EOF.strip_heredoc
-          foo: bar
-          multiline_string: is
-           appropriately
-           indented
-          another_key: value
-        EOF
-      )
-    end
-  end
-
   describe '#generate_password' do
     it 'generates a string of uppercase, lowercase, or digit characters' do
       expect(
