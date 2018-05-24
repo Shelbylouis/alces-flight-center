e9d2bc1742c0dcd1cc92a4c61e7f6788fde9c5dd
James Muscat <james.muscat@alces-software.com>
2018-05-24 17:39:11 +0100

    Parse a start date (hence a quarter) from the URL and use it.

--- app/controllers/clusters_controller.rb
+++ app/controllers/clusters_controller.rb
@@ -2,7 +2,42 @@ class ClustersController < ApplicationController
   decorates_assigned :cluster
 
   def credit_usage
-    @accrued = 0
-    @used = 0
+
+    @events = (charges + deposits).sort_by(&:created_at)
+
+    @accrued = deposits.reduce(0) do |total, deposit|
+      total += deposit.amount
+    end
+
+    @used = charges.reduce(0) do |total, kase|
+      total += kase.credit_charge.amount
+    end
+
+  end
+
+  private
+
+  def start_date
+    @start_date ||= begin
+      parse_start_date.beginning_of_quarter
+    rescue
+      Date.today.beginning_of_quarter
+    end
+  end
+
+  def end_date
+    @end_date ||= start_date.end_of_quarter
+  end
+
+  def parse_start_date
+    Date.parse(params[:start_date])
+  end
+
+  def charges
+    @cluster.cases.with_charge_in_period(start_date, end_date)
+  end
+
+  def deposits
+    @cluster.credit_deposits.in_period(start_date, end_date)
   end
 end
