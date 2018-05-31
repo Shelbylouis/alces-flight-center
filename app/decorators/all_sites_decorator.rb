67a30290e564370a424f13f26a284c6f7e580f1d
Andrew <andrew.toghill@alces-software.com>
2018-05-31 10:10:11 +0100

    Use the rails helper for the case paths
    As the decorators have access to the Rails helpers I can access the
    url_helpers via 'h'

--- app/decorators/all_sites_decorator.rb
+++ app/decorators/all_sites_decorator.rb
@@ -9,7 +9,7 @@ class AllSitesDecorator < ApplicationDecorator
   end
 
   def dashboard_case_path(kase)
-    Rails.application.routes.url_helpers.cluster_case_path(kase.cluster, kase)
+    h.cluster_case_path(kase.cluster, kase)
   end
 
   private
