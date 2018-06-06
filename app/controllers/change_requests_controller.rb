11f514a32715be6c4461ba29a7d34fb4aff0edd2
James Muscat <james.muscat@alces-software.com>
2018-06-06 13:56:32 +0100

    Create CR.

--- app/controllers/change_requests_controller.rb
+++ app/controllers/change_requests_controller.rb
@@ -7,13 +7,18 @@ class ChangeRequestsController < ApplicationController
   end
 
   def create
-    p params
+    @case.create_change_request!(cr_params)
+    redirect_to case_path(@case.display_id)
   end
 
   private
 
   def assign_case
     @case = Case.find_from_id!(params.require(:case_id)).decorate
+  end
+
+  def cr_params
+    params.require(:change_request).permit(:details, :credit_charge)
   end
 
 end
