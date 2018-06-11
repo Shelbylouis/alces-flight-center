109328896444bf29d7b985f947ed7e056ebbe3e8
James Muscat <james.muscat@alces-software.com>
2018-06-11 16:52:39 +0100

    Restrict possible `amount`s for `CreditDeposit`/`CreditCharge`.

--- app/models/credit_deposit.rb
+++ app/models/credit_deposit.rb
@@ -2,4 +2,9 @@ class CreditDeposit < CreditEvent
   belongs_to :cluster
   delegate :site, to: :cluster
   attr_readonly :cluster
+
+  validates :amount, numericality: {
+    minimum: 1,
+    only_integer: true,
+  }
 end
