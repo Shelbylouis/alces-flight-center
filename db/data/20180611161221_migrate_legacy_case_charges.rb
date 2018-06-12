474bc23959a3459bf2735644cc8cdbf54a6cdbd8
James Muscat <james.muscat@alces-software.com>
2018-06-12 16:01:05 +0100

    Include user who closed the case in migrated `CreditCharge`s.

--- db/data/20180611161221_migrate_legacy_case_charges.rb
+++ db/data/20180611161221_migrate_legacy_case_charges.rb
@@ -2,7 +2,11 @@ class MigrateLegacyCaseCharges < ActiveRecord::Migration[5.2]
   def up
     Case.where(state: 'closed').each do |k|
       unless k.credit_charge.present?
-        k.create_credit_charge(amount: k.legacy_credit_charge)
+
+        # If a Case is closed, it ought to have a transition to that state!
+        closing_user = k.transitions.find_by(to: 'closed').user
+
+        k.create_credit_charge!(amount: k.legacy_credit_charge, user: closing_user)
       end
     end
   end
