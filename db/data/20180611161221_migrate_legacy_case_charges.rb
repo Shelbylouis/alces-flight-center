322cd9b93e943b2794e06e94495759064c5f253c
James Muscat <james.muscat@alces-software.com>
2018-06-12 16:53:53 +0100

    Remove original `credit_charge` audits once new `CreditCharge` created.

--- db/data/20180611161221_migrate_legacy_case_charges.rb
+++ db/data/20180611161221_migrate_legacy_case_charges.rb
@@ -3,10 +3,23 @@ class MigrateLegacyCaseCharges < ActiveRecord::Migration[5.2]
     Case.where(state: 'closed').each do |k|
       unless k.credit_charge.present?
 
-        # If a Case is closed, it ought to have a transition to that state!
-        closing_user = k.transitions.find_by(to: 'closed').user
+        audits = k.audits.where(action: 'update').all
 
-        k.create_credit_charge!(amount: k.legacy_credit_charge, user: closing_user)
+        charge_audit = audits.select { |audit|
+          audit.audited_changes.include? 'credit_charge'
+        }.last
+
+        k.create_credit_charge!(
+          amount: k.legacy_credit_charge,
+          user: charge_audit.user,
+          created_at: charge_audit.created_at
+        )
+
+        # We want to delete the original audit because:
+        #  - all its relevant information is now captured in CreditCharge;
+        #  - if we don't then duplicate "charge added" entries may appear in
+        #    a Case's events feed even though the charge only gets added once
+        charge_audit.delete
       end
     end
   end
