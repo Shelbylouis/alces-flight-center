6c349e3b6d241c914b0f8c813eaf3ae49243b5a8
Bob Whitelock <bob.whitelock1@gmail.com>
2018-06-13 13:38:36 +0100

    Disable reject maintenance button for viewers

--- spec/features/maintenance_windows_spec.rb
+++ spec/features/maintenance_windows_spec.rb
@@ -472,11 +472,19 @@ RSpec.feature "Maintenance windows", type: :feature do
       )
     end
 
+    let(:path) { cluster_maintenance_windows_path(cluster, as: user) }
+
     it_behaves_like 'button is disabled for viewers', button_link: true do
-      let(:path) { cluster_maintenance_windows_path(cluster, as: user) }
       let(:button_text) { confirm_button_link_text }
       let(:disabled_button_title) do
         'As a viewer you cannot confirm maintenance'
+      end
+    end
+
+    it_behaves_like 'button is disabled for viewers' do
+      let(:button_text) { reject_button_text }
+      let(:disabled_button_title) do
+        'As a viewer you cannot reject maintenance'
       end
     end
   end
