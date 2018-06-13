63f74813563a90409f5676b2f63f9f5aaa1ba570
Bob Whitelock <bob.whitelock1@gmail.com>
2018-06-13 13:49:30 +0100

    Break support type button tests into contexts
    In preparation for adding additional tests in the same contexts to
    these.

--- spec/support/shared_examples/support_type_change_buttons.rb
+++ spec/support/shared_examples/support_type_change_buttons.rb
@@ -19,44 +19,56 @@ RSpec.shared_examples 'can request support_type change via buttons' do |part_nam
     send("cluster_#{part_name.to_s.pluralize}_path", cluster, as: contact)
   end
 
-  it "can request self-management of a managed #{part_class_name}" do
-    part = create("managed_#{part_name}", cluster: cluster)
+  context "when #{part_class_name} is managed" do
+    let! :part do
+      create("managed_#{part_name}", cluster: cluster)
+    end
 
-    visit cluster_parts_path
-    click_button 'Request self-management'
+    it 'can request self-management' do
+      visit cluster_parts_path
+      click_button 'Request self-management'
 
-    created_case = part.cases.first
-    expect(created_case.send(part_name)).to eq(part)
-    issue = Issue.send("request_#{part_name}_becomes_advice_issue")
-    expect(created_case.issue).to eq(issue)
-    tier = issue.tiers.first
-    expect(created_case.fields).to eq(tier.fields)
-    expect(created_case.tier_level).to eq(tier.level)
+      created_case = part.cases.first
+      expect(created_case.send(part_name)).to eq(part)
+      issue = Issue.send("request_#{part_name}_becomes_advice_issue")
+      expect(created_case.issue).to eq(issue)
+      tier = issue.tiers.first
+      expect(created_case.fields).to eq(tier.fields)
+      expect(created_case.tier_level).to eq(tier.level)
+    end
   end
 
-  it "can request Alces management of an advice-only #{part_class_name}" do
-    part = create("advice_#{part_name}", cluster: cluster)
+  context "when #{part_class_name} is advice-only" do
+    let! :part do
+      create("advice_#{part_name}", cluster: cluster)
+    end
 
-    visit cluster_parts_path
-    click_button request_alces_management_text
+    it "can request Alces management" do
+      visit cluster_parts_path
+      click_button request_alces_management_text
 
-    created_case = part.cases.first
-    expect(created_case.send(part_name)).to eq(part)
-    issue = Issue.send("request_#{part_name}_becomes_managed_issue")
-    expect(created_case.issue).to eq(issue)
-    tier = issue.tiers.first
-    expect(created_case.fields).to eq(tier.fields)
-    expect(created_case.tier_level).to eq(tier.level)
+      created_case = part.cases.first
+      expect(created_case.send(part_name)).to eq(part)
+      issue = Issue.send("request_#{part_name}_becomes_managed_issue")
+      expect(created_case.issue).to eq(issue)
+      tier = issue.tiers.first
+      expect(created_case.fields).to eq(tier.fields)
+      expect(created_case.tier_level).to eq(tier.level)
+    end
   end
 
-  it "displays no buttons for internal #{part_class_name}" do
-    part = create(part_name, internal: true, cluster: cluster)
+  context "when #{part_class_name} is internal" do
+    let! :part do
+      create(part_name, internal: true, cluster: cluster)
+    end
 
-    visit cluster_parts_path
-    # Sanity check that part itself is actually being displayed.
-    expect(page).to have_text(part.name)
+    it 'displays no buttons' do
+      visit cluster_parts_path
+      # Sanity check that part itself is actually being displayed.
+      expect(page).to have_text(part.name)
 
-    expect(page).not_to have_button(request_self_management_text)
-    expect(page).not_to have_button(request_alces_management_text)
+      expect(page).not_to have_button(request_self_management_text)
+      expect(page).not_to have_button(request_alces_management_text)
+    end
   end
 end
