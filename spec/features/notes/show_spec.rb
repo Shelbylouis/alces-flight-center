de165c24769648c9bb991c292d444b3db39a0bfa
Bob Whitelock <bob.whitelock1@gmail.com>
2018-06-15 16:08:17 +0100

    Hide textarea in new note form for viewers
    The new note form is rendered for the `show` action when there are no
    notes of this flavour already. Previously viewers could fill in the
    textarea but not submit the form as the button is disabled; now the
    textarea is not shown to viewers at all, to make clear both that there
    are no notes currently and that they cannot add any.

--- spec/features/notes/show_spec.rb
+++ spec/features/notes/show_spec.rb
@@ -6,9 +6,35 @@ RSpec.describe 'Show notes page', type: :feature do
 
   let(:path) { cluster_notes_path(cluster, :customer, as: user) }
 
+  before :each do
+    visit path
+  end
+
   # This context actually tests what is rendered by `new` view, which is
   # rendered by `show` action when there are no notes already.
   context 'when no notes already' do
+    let(:description) { :note_description }
+
+    context 'for contact' do
+      let(:user) { create(:contact, site: site) }
+
+      it 'has textarea for note description' do
+        expect do
+          fill_in(description, with: 'words')
+        end.not_to raise_error
+      end
+    end
+
+    context 'for viewer' do
+      let(:user) { create(:viewer, site: site) }
+
+      it 'does not have textarea for note description' do
+        expect do
+          fill_in(description, with: 'words')
+        end.to raise_error(Capybara::ElementNotFound)
+      end
+    end
+
     it_behaves_like 'button is disabled for viewers' do
       let(:button_text) { 'Create notes' }
       let(:disabled_button_title) do
