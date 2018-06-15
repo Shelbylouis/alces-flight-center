69e5683e14857eced437b02c0a14455bff817973
Bob Whitelock <bob.whitelock1@gmail.com>
2018-06-15 15:47:52 +0100

    Disable edit notes button for viewers

--- spec/features/notes/show_spec.rb
+++ spec/features/notes/show_spec.rb
@@ -1,14 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe 'Show notes page', type: :feature do
-  it_behaves_like 'button is disabled for viewers' do
-    let(:site) { create(:site) }
-    let(:cluster) { create(:cluster, site: site) }
+  let(:site) { create(:site) }
+  let(:cluster) { create(:cluster, site: site) }
 
-    let(:path) { cluster_notes_path(cluster, :customer, as: user) }
-    let(:button_text) { 'Create notes' }
-    let(:disabled_button_title) do
-      'As a viewer you cannot create cluster notes'
+  let(:path) { cluster_notes_path(cluster, :customer, as: user) }
+
+  # This context actually tests what is rendered by `new` view, which is
+  # rendered by `show` action when there are no notes already.
+  context 'when no notes already' do
+    it_behaves_like 'button is disabled for viewers' do
+      let(:button_text) { 'Create notes' }
+      let(:disabled_button_title) do
+        'As a viewer you cannot create cluster notes'
+      end
+    end
+  end
+
+  context 'when cluster already has notes' do
+    before :each do
+      create(:customer_note, cluster: cluster)
+    end
+
+    it_behaves_like 'button is disabled for viewers', button_link: true do
+      let(:button_text) { 'Edit notes' }
+      let(:disabled_button_title) do
+        'As a viewer you cannot edit cluster notes'
+      end
     end
   end
 end
