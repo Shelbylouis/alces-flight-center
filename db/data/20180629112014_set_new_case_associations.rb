992732f0b4ecbb943dec2b50823f996466e1bee2
James Muscat <james.muscat@alces-software.com>
2018-07-02 16:27:55 +0100

    Let data migration continue to work now we've deleted old associations.

--- db/data/20180629112014_set_new_case_associations.rb
+++ db/data/20180629112014_set_new_case_associations.rb
@@ -1,4 +1,25 @@
 class SetNewCaseAssociations < ActiveRecord::Migration[5.2]
+
+  class Case < ApplicationRecord
+    # Minimal set of relationships required for this migration to function.
+    # Note that the belongs_to relationships are deleted in the model so must
+    # be specified here.
+    belongs_to :service, required: false
+    belongs_to :component, required: false
+
+    has_many :case_associations
+    has_many :services,
+             through: :case_associations,
+             source: :associated_element,
+             source_type: 'Service'
+
+    has_many :components,
+             through: :case_associations,
+             source: :associated_element,
+             source_type: 'Component'
+
+  end
+
   def up
     Case.all.each do |kase|
 
