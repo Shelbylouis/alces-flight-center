29aaaf69cac8aa82cb339fa742ffbdb30847f008
Andrew <andrew.toghill@alces-software.com>
2018-07-06 16:12:33 +0100

    Authorise the check_submission method

--- app/policies/cluster_policy.rb
+++ app/policies/cluster_policy.rb
@@ -2,6 +2,7 @@ class ClusterPolicy < ApplicationPolicy
 
   alias_method :deposit?, :admin?
   alias_method :check_results?, :admin?
+  alias_method :check_submission?, :admin?
 
   class Scope < Scope
     def resolve
