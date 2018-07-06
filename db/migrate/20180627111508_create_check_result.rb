0dd49a912f3b6f7d2895d2f867ff448176b34554
Andrew <andrew.toghill@alces-software.com>
2018-07-06 10:15:34 +0100

    Update user reference to be a foreign key

--- db/migrate/20180627111508_create_check_result.rb
+++ db/migrate/20180627111508_create_check_result.rb
@@ -3,7 +3,7 @@ class CreateCheckResult < ActiveRecord::Migration[5.2]
     create_table :check_results do |t|
       t.references :cluster_check, null: false
       t.date :date, null: false
-      t.references :user, null: false
+      t.references :user, foreign_key: true, null: false
       t.string :result, null: false
       t.string :comment, null: true
       t.references :log, null: true
