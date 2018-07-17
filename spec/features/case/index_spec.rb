9fc53f993a0bde6a4a184883c08634f4be3b8ece
James Muscat <james.muscat@alces-software.com>
2018-07-17 17:44:40 +0100

    Add some tests for case filtering.

--- spec/features/case/index_spec.rb
+++ spec/features/case/index_spec.rb
@@ -81,4 +81,64 @@ RSpec.describe 'Cases table', type: :feature do
       include_examples 'open cases table rendered'
     end
   end
+
+  describe 'filtering' do
+    let(:component1) { create(:component, cluster: cluster) }
+    let(:component2) { create(:component, cluster: cluster) }
+
+    let!(:component_case_1) {
+      create(
+        :open_case,
+        cluster: cluster,
+        components: [component1],
+        subject: 'CC1',
+        assignee: admin
+      )
+    }
+    let!(:component_case_2) {
+      create(
+        :open_case,
+        cluster: cluster,
+        components: [component2],
+        subject: 'CC2',
+        assignee: admin
+      )
+    }
+    let!(:component_case_3) {
+      create(
+        :open_case,
+        cluster: cluster,
+        components: [component1],
+        subject: 'CC3'
+      )
+    }
+
+    it 'filters by assignee' do
+      visit cases_path(assigned_to: [admin], as: contact)
+      cases = all('tr').map(&:text)
+      expect(cases).to have_text('CC1')
+      expect(cases).to have_text('CC2')
+      expect(cases).not_to have_text('CC3')
+    end
+
+    it 'filters by association' do
+      visit cases_path(associations: ["Component-#{component1.id}"], as: contact)
+      cases = all('tr').map(&:text)
+      expect(cases).to have_text('CC1')
+      expect(cases).not_to have_text('CC2')
+      expect(cases).to have_text('CC3')
+    end
+
+    it 'works with multiple active filters' do
+      visit cases_path(
+        assigned_to: [admin],
+        associations: ["Component-#{component1.id}"],
+        as: contact
+      )
+      cases = all('tr').map(&:text)
+      expect(cases).to have_text('CC1')
+      expect(cases).not_to have_text('CC2')
+      expect(cases).not_to have_text('CC3')
+    end
+  end
 end
