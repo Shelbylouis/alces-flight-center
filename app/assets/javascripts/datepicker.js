cff1db6a526978d1d2ffeecbbb2f264e402630fa
Andrew <andrew.toghill@alces-software.com>
2018-07-19 16:21:09 +0100

    Remove highlighting of weekdays
    It should hopefully be obvious enough which days can be accessed and
    with the previous change making future weekdays also disabled this
    became a bit misleading.

--- app/assets/javascripts/datepicker.js
+++ app/assets/javascripts/datepicker.js
@@ -3,7 +3,6 @@ function initialiseDatepicker() {
     format: "yyyy-mm-dd",
     endDate: '+0d',
     daysOfWeekDisabled: [0,6],
-    daysOfWeekHighlighted: [1,2,3,4,5],
     todayHighlight: true,
     todayBtn: "linked",
     orientation: "bottom"
