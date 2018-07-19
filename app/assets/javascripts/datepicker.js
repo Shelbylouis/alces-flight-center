cfaf3ae82f833c017260ec885ad0127262dd548b
Andrew <andrew.toghill@alces-software.com>
2018-07-19 12:20:08 +0100

    Initialise datepicker elements
    The initial (heh) initialisation options for the datepicker to be used
    on the check results page, allowing users to view check results for a
    specific date.
    
    These options format the date and disable days that checks are not
    handled.

---
+++ app/assets/javascripts/datepicker.js
@@ -0,0 +1,13 @@
+function initialiseDatepicker() {
+  $("#datepicker").datepicker({
+    format: "yyyy-mm-dd",
+    daysOfWeekDisabled: [0,6],
+    daysOfWeekHighlighted: [1,2,3,4,5],
+    todayHighlight: true,
+    todayBtn: "linked",
+    orientation: "bottom"
+  });
+}
+
+
+document.addEventListener('turbolinks:load', initialiseDatepicker);
