b35928e3367de612e07ef7df4838cedae867bd62
James Muscat <james.muscat@alces-software.com>
2018-07-24 18:41:00 +0100

    Use `effective_date` for `in_period` scope for `CreditCharge`.

--- app/models/credit_deposit.rb
+++ app/models/credit_deposit.rb
@@ -7,4 +7,10 @@ class CreditDeposit < CreditEvent
     greater_than_or_equal_to: 1,
     only_integer: true,
   }
+
+  # When we add `effective_date`s to deposits, this and the equivalent method in
+  # CreditCharge can be pulled up into CreditEvent
+  scope :in_period, lambda { |start_date, end_date|
+    where(created_at: start_date..end_date)
+  }
 end
--- app/models/credit_event.rb
+++ app/models/credit_event.rb
@@ -11,7 +11,4 @@ class CreditEvent < ApplicationRecord
 
   attr_readonly :user, :amount
 
-  scope :in_period, lambda { |start_date, end_date|
-    where(created_at: start_date..end_date)
-  }
 end
