76f120fdb021e528c60dc414b060175a9ad56d38
James Muscat <james.muscat@alces-software.com>
2018-07-25 11:13:40 +0100

    Set a `:long` format for `Date` to match that for `Time`.

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,6 +1,9 @@
 
 Time::DATE_FORMATS[:long] = lambda do |time|
-  time.strftime("%A #{time.day.ordinalize} %B, %Y, %H:%M%P")
+  time.strftime("%A, #{time.day.ordinalize} %B %Y, %H:%M%P")
+end
+Date::DATE_FORMATS[:long] = lambda do |time|
+  time.strftime("%A, #{time.day.ordinalize} %B %Y")
 end
 
 Time::DATE_FORMATS[:short] = "%a %d %b %H:%M"
