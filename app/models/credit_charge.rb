c81bbe8aa2c5dbeee053d1995fca6b3f7b7580e9
James Muscat <james.muscat@alces-software.com>
2018-07-25 16:51:57 +0100

    Data migration to set `effective_date` on `CreditDeposit`s.

--- app/models/credit_charge.rb
+++ app/models/credit_charge.rb
@@ -1,7 +1,7 @@
 class CreditCharge < CreditEvent
   belongs_to :case
   delegate :site, to: :case
-  attr_readonly :case, :effective_date
+  attr_readonly :case
 
   validates :amount, numericality: {
     greater_than_or_equal_to: 0,
--- app/models/credit_event.rb
+++ app/models/credit_event.rb
@@ -11,7 +11,9 @@ class CreditEvent < ApplicationRecord
 
   validates :effective_date, presence: true, if: :persisted?
 
-  attr_readonly :user, :amount, :effective_date
+  # TODO set :effective_date as readonly once data migrations have happened?
+  # (Or maybe Mark wants to be able to tweak this in the future...)
+  attr_readonly :user, :amount  # , :effective_date
 
   scope :in_period, lambda { |start_date, end_date|
     where(effective_date: start_date..end_date)
