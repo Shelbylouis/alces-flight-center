c4762b51057eae6b8133f4c610a9d892b4cd4f48
Andrew <andrew.toghill@alces-software.com>
2018-07-25 15:16:03 +0100

    Authorise new methods within controller
    These methods will be used to facilitate Markdown support

--- app/policies/cluster_policy.rb
+++ app/policies/cluster_policy.rb
@@ -3,6 +3,9 @@ class ClusterPolicy < ApplicationPolicy
   alias_method :deposit?, :admin?
   alias_method :check_results?, :admin?
   alias_method :check_submission?, :admin?
+  alias_method :preview?, :admin?
+  alias_method :write?, :admin?
+  alias_method :create?, :admin?
 
   class Scope < Scope
     def resolve
