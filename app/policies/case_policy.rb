7e598321759179e6adc2faec1d9bc29ba9fb530d
James Muscat <james.muscat@alces-software.com>
2018-07-27 16:52:26 +0100

    Magical appearing/disappearing subject edit form.

--- app/policies/case_policy.rb
+++ app/policies/case_policy.rb
@@ -8,6 +8,7 @@ class CasePolicy < ApplicationPolicy
   alias_method :set_time?, :admin?
   alias_method :set_commenting?, :admin?
   alias_method :edit_associations?, :admin?
+  alias_method :edit?, :admin?
 
   def redirect_to_canonical_path?
     true
