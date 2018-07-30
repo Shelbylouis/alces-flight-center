86623d7b983859185f198ed6c68218d99815a7f0
Andrew <andrew.toghill@alces-software.com>
2018-07-30 11:37:02 +0100

    Stop delegating id for cluster checks
    This was a bad idea

--- app/models/cluster_check.rb
+++ app/models/cluster_check.rb
@@ -4,7 +4,6 @@ class ClusterCheck < ApplicationRecord
 
   has_many :check_results
 
-  delegate :id, to: :check, allow_nil: true
   delegate :name, to: :check, allow_nil: true
   delegate :check_category, to: :check
   delegate :site, to: :cluster
