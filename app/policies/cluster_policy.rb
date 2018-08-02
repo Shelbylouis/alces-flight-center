7b42bcf7158c8c0752d34d251d7540633bd3f670
Andrew <andrew.toghill@alces-software.com>
2018-08-02 15:41:53 +0100

    Use a more appropriate policy

--- app/policies/cluster_policy.rb
+++ app/policies/cluster_policy.rb
@@ -7,7 +7,6 @@ class ClusterPolicy < ApplicationPolicy
   alias_method :preview?, :admin?
   alias_method :write?, :admin?
   alias_method :create?, :admin?
-  alias_method :authorise?, :admin?
 
   class Scope < Scope
     def resolve
