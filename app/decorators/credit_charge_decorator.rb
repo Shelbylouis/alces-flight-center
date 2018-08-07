59f14c6fc46fdd5e2f3a7c792a20821b73772042
DavidMarchant <davidwjmarchant@gmail.com>
2018-08-07 12:03:51 +0100

    Add basic time calulation & basic outputing
    - no formatting

--- app/decorators/credit_charge_decorator.rb
+++ app/decorators/credit_charge_decorator.rb
@@ -4,10 +4,28 @@ class CreditChargeDecorator < ApplicationDecorator
   def credit_usage_card
     kase = object.case.decorate
     link_text = "#{kase.display_id} - #{kase.subject}"
-
+    time_to_response = 0
+    first_admin_comment = nil
+    #if an admin created the case, time to response is 0
+    if !kase.user.admin?
+      kase.events.reverse.each do |event|
+        if event.instance_of?(CaseComment)
+          if event.user.admin?
+            first_admin_comment = event
+            break
+          end
+        end
+      end
+      if first_admin_comment.nil?
+        time_to_response = "N/A" 
+      else
+        time_to_response = kase.created_at.business_time_until(first_admin_comment.created_at)
+      end
+    end
     h.render 'clusters/credit_charge_entry',
              amount: -amount,
-             date: effective_date do
+             date: effective_date,
+             time_to_response: time_to_response do
       h.link_to link_text, h.case_path(kase), class: h.credit_value_class(-amount)
     end
   end
