95d6eea702b912b2529d8e013acffbd80d172051
Andrew <andrew.toghill@alces-software.com>
2018-08-07 15:34:57 +0100

    Define datepickers for various use cases
    The required datepickers for Checks, Credit Usage and Maintenance
    Windows all have different needs, Therefore each one has a separately
    defined datepicker with mostly unique intialisation.
    
    Having them defined in this way allows for easier individual
    customisation but is not 100% necessary. The changes required for all 3
    can be handled by calling methods on the datepicker to adjust the
    settings.
    
    Also in this commit is a change to reflect the new id name for the
    currently used datepicker for Checks.

--- app/assets/javascripts/datepicker.js
+++ app/assets/javascripts/datepicker.js
@@ -1,10 +1,31 @@
 function initialiseDatepicker() {
-  $("#datepicker").datepicker({
+  // Having a datepicker defined for each use case separately isn't necessary
+  // as there are methods you can call to handle changing the disabled days for
+  // example. However for now this offers the ability for easier individual
+  // customisation if we need it.
+
+  $("#restricted-datepicker").datepicker({
     format: "yyyy-mm-dd",
     endDate: '+0d',
     daysOfWeekDisabled: [0,6],
     todayHighlight: true,
     todayBtn: "linked",
+    orientation: "bottom"
+  });
+
+  $("#credit-datepicker").datepicker({
+    format: "yyyy-mm-dd",
+    endDate: '+0d',
+    todayHighlight: true,
+    todayBtn: "linked",
+    orientation: "bottom"
+  });
+
+  $("#maintenance-datepicker").datepicker({
+    format: "yyyy-mm-dd",
+    todayHighlight: true,
+    todayBtn: "linked",
+    autoclose: true,
     orientation: "bottom"
   });
 }
