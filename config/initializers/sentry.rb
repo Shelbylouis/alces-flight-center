bdf6ee92a42a3bf19e86af900be027f704eab988
James Muscat <james.muscat@alces-software.com>
2018-08-07 12:07:02 +0100

    Use async `ActiveJob`s to send exceptions to Sentry.
    This is suggested config from
    https://docs.sentry.io/clients/ruby/config/#optional-settings, and I
    think it might solve the problem we've encountered previously where
    Sentry leaks exceptions from one thread to another (see Trello card at
    https://trello.com/c/wC2DpSaQ/390-use-async-jobs-to-send-errors-to-sentry
    for where I think this has happened).

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,3 +1,11 @@
+class SentryJob < ActiveJob::Base
+  queue_as :default
+
+  def perform(event)
+    Raven.send_event(event)
+  end
+end
+
 
 if ENV.has_key?('SENTRY_DSN')
   Raven.configure do |config|
@@ -5,5 +13,9 @@ if ENV.has_key?('SENTRY_DSN')
 
     # Do not send any filtered parameters to Sentry.
     config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
+
+    config.async = lambda { |event|
+      SentryJob.perform_later(event.to_hash)
+    }
   end
 end
