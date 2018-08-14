808fce27e617c145125af2505dab9ac0853d3379
James Muscat <james.muscat@alces-software.com>
2018-08-14 16:57:08 +0100

    Test for sorting cases by priority / calculating TSLAU.

--- spec/features/case/index_spec.rb
+++ spec/features/case/index_spec.rb
@@ -155,4 +155,53 @@ RSpec.describe 'Cases table', type: :feature do
       expect(cases).not_to have_text('CC3')
     end
   end
+
+  describe '#prioritised' do
+    include ActiveSupport::Testing::TimeHelpers
+    let(:cluster2) { create(:cluster) }
+    let!(:cluster2_contact) { create(:contact, site: cluster2.site )}
+
+    let(:kase1) { create(:open_case, cluster: cluster2, subject: 'Kase1') }
+    let(:kase2) { create(:open_case, cluster: cluster2, subject: 'Kase2') }
+    let(:kase3) { create(:open_case, cluster: cluster2, subject: 'Kase3') }
+
+    it 'sorts cases by last update' do
+      travel_to Time.zone.local(2018, 8, 13, 9, 0, 0) do
+        # Create all cases at 9am
+        kase1.reload
+        kase2.reload
+        kase3.reload
+      end
+
+      travel_to Time.zone.local(2018, 8, 13, 10, 30, 0) do
+        visit cluster_case_path(cluster2, kase1, as: admin)
+        fill_in 'case_comment_text', with: 'Response!'
+        click_button 'Add new comment'
+      end
+
+      travel_to Time.zone.local(2018, 8, 13, 12, 30, 0) do
+        visit cluster_case_path(cluster2, kase2, as: admin)
+        fill_in 'case_comment_text', with: 'Response!'
+        click_button 'Add new comment'
+      end
+
+      travel_to Time.zone.local(2018, 8, 13, 14, 0, 0) do
+        visit cluster_cases_path(cluster2, prioritised: true, as: admin)
+        cases = all('tr').map(&:text)
+
+        # Is the order correct?
+        expect(cases[1]).to have_text('Kase3')
+        expect(cases[2]).to have_text('Kase1')
+        expect(cases[3]).to have_text('Kase2')
+
+        # What about the durations?
+        expect(cases[1]).to have_text 'None None'  # Affected components also says 'None'
+        expect(cases[2]).to have_text '3h 30m'
+        expect(cases[3]).to have_text '1h 30m'
+
+      end
+
+    end
+
+  end
 end
