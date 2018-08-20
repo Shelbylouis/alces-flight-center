da6eecd0a83157dcf1a874ed057583d79bdc6751
James Muscat <james.muscat@alces-software.com>
2018-08-20 12:16:22 +0100

    Keep data migration working now columns have been renamed.

--- db/data/20180816102151_set_initial_note_titles.rb
+++ db/data/20180816102151_set_initial_note_titles.rb
@@ -1,4 +1,8 @@
 class SetInitialNoteTitles < ActiveRecord::Migration[5.2]
+
+  class Note < ApplicationRecord
+  end
+
   def up
     Note.all.each do |note|
       note.title = note.flavour == 'customer' ? 'Customer notes' : 'Engineering notes'
