6b54b6dce12a79700c8a888f447ded782ea560ad
James Muscat <james.muscat@alces-software.com>
2018-08-29 16:21:32 +0100

    Create an administrative issue with tier-2 details field.

---
+++ db/data/20180829150212_add_administrative_issue.rb
@@ -0,0 +1,22 @@
+class AddAdministrativeIssue < ActiveRecord::Migration[5.2]
+
+  ADMIN_ISSUE_NAME = 'Administrative'.freeze
+
+  def up
+    issue = Issue.create!(
+      name: ADMIN_ISSUE_NAME,
+      administrative: true,
+      requires_component: false,
+      requires_service: false,
+      service_type: nil
+    )
+
+    tier = issue.tiers.first
+    tier.level = 2
+    tier.save!
+  end
+
+  def down
+    Issue.find_by_name(ADMIN_ISSUE_NAME).destroy!
+  end
+end
