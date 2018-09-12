4c7b0f23534cc00e511eb28621da3312f6563bbd
James Muscat <james.muscat@alces-software.com>
2018-09-12 11:50:33 +0100

    Test with a longer, more Bel Air comment.
    Lorem Ipsum is boring.

--- spec/features/autoresize_spec.rb
+++ spec/features/autoresize_spec.rb
@@ -6,17 +6,32 @@ RSpec.describe 'Autoresize', type: :feature, js: true do
   let(:admin) { create(:admin) }
   let(:kase) { create(:open_case, cluster: cluster, assignee: admin) }
   let(:content) {
-    <<~EOF.squish
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
-      sollicitudin luctus lacinia. Phasellus ultricies eros ac neque molestie
-      aliquet. Vestibulum convallis elementum sodales. Praesent sed nisl est.
-      Sed imperdiet in massa nec maximus. Suspendisse dignissim aliquet sem,
-      quis placerat ex condimentum eget. Nulla commodo efficitur venenatis. Sed
-      cursus dictum sapien et consectetur. Duis id vestibulum diam. Suspendisse
-      pulvinar ex vitae augue ultricies convallis. Donec vestibulum finibus sem
-      ac vestibulum. Integer et sem vel libero interdum bibendum id ut ex. Nam
-      eget lorem justo.
-    EOF
+    <<~EOF
+Now this is a story all about how
+My life got flipped, turned upside-down
+And I’d like to take a minute, just sit right there
+I’ll tell you how I became a guy typing into an auto-expanding textbox area
+
+In west Javascriptia, born and raised
+With a Webpack was where I spent most of my days
+Chilling out, maxing, compiling all cool an’ all
+Shooting some bugfixes outside of school
+When a couple of guys, they were up to no good
+Started writing Elm into the neighbourhood
+I got one little exception and Mark got scared
+He said, “You’re moving on to work on Clusterware”
+
+I whistled for a cab, and when it came near
+The licence plate said “BASH” and it had Ruan in there
+If anything I’d say that this cab was rare
+But I thought, “Nah, forget it -
+Yo, home, to Bicester!”
+
+I pulled up to a house on the Murdock estate
+And I yelled out to Ruan, “Yo, home, smell you later!”
+I looked at my kingdom, I was finally there
+Typing into an auto-expanding textbox area.
+EOF
   }
 
   def textarea_height
@@ -27,6 +42,15 @@ RSpec.describe 'Autoresize', type: :feature, js: true do
     visit cluster_case_path(cluster, kase, as: admin)
 
     initial_height = textarea_height
+
+    # Since the autoresize script takes account of position in the browser
+    # viewport, we need to make sure it's got space to expand by scrolling it
+    # into view before "typing" into it.
+    Capybara.current_session.driver.execute_script(
+      'arguments[0].scrollIntoView();',
+      find('#case_comment_text').native
+    )
+
     fill_in 'case_comment_text', with: content
 
     expect(textarea_height).to be > initial_height
